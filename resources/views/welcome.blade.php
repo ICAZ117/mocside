0bdae86bc4afda9cf5c18d401a17f38d7320086f
ICAZ117 <ibraheemcazalas@gmail.com>
2021-05-18 12:52:09 -0400

    Added images and set up head tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,16 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{env('APP_NAME')}} | pageName</title>
+        <link rel="shortcut icon" href="img/icon.png">
+        <!-- Bootstrap 4.5 CSS -->
+        <link rel="stylesheet" href="{{ mix('css/bootstrap.min.css') }}">
+        <!-- Style CSS -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <title>{{env('APP_NAME')}}</title>
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
     </head>
     <body>
         <div id="app">
