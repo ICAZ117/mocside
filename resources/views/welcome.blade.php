aca7ab240b0e290684171c0506a5fa48e5d769d3
ICAZ117 <ibraheemcazalas@gmail.com>
2021-05-18 12:45:00 -0400

    Renaming style.css to app.css to fix error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="{{ mix('css/style.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <title>{{env('APP_NAME')}}</title>
     </head>
     <body>
