d7d4668ce3d10ed509534ca8171945b50006cb47
Vbuddies <cheesychase@myexcel.com>
2021-05-18 12:02:27 -0400

    mispelled

--- resources/js/Router/index.js
+++ resources/js/Router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from "./routes";
 
 const router = createRouter({
-    histroy: createWebHistory(),
+    history: createWebHistory(),
     routes,
 })
 
