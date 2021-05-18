81b596791dda6a3e99584add98da71cd666dc877
Vbuddies <cheesychase@myexcel.com>
2021-05-18 11:54:31 -0400

    added .. to source file path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,6 @@
             <app></app>
         </div>
 
-        <script src="{{ mix('./js/app.js') }}"></script>
+        <script src="{{ mix('../js/app.js') }}"></script>
     </body>
 </html>
