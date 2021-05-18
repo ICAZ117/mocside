8a02213b572ee938cdfca9c0972aa0e950a5d5d1
maxb00 <maxabr00@yahoo.com>
2021-05-18 12:14:30 -0400

    edited app.js, begin fortify

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,15 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    public function getKey(): string {
+        return static::class;
+    }
 
     /**
      * The attributes that are mass assignable.
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,19 @@
 require('./bootstrap');
 
-import Vue from 'vue';
+import { createApp } from 'vue';
 import VueRouter from 'vue-router';
 
 import router from "./Router/index";
 import App from "./App.vue";
 
-Vue.use(VueRouter);
+// Vue.use(VueRouter);
 
-const app = new Vue({
-    el: '#app',
-    router,
-    components: { App }
-})
+// const app = new Vue({
+//     el: '#app',
+//     router,
+//     components: { App }
+// })
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
