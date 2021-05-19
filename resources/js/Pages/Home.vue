54991c85e96aef8d66b7b3eec46a5e3bbaaa8236
ICAZ117 <ibraheemcazalas@gmail.com>
2021-05-19 13:42:18 -0400

    Added login form to modals in home and about pages

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -99,7 +99,51 @@
         Login
     </template>
     <template v-slot:body>
-        <p>Email</p>
+        <!-- Login Form -->
+    <div class="login">
+        <form action="" class="needs-validation" novalidate>
+            <div class="form-group">
+                <label for="uname">Username:</label>
+                <input type="text" class="form-control" id="uname" placeholder="Enter username" name="uname" required>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback">Please enter your username</div>
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback">Please enter your password</div>
+            </div>
+            <div class="form-group form-check">
+                <label class="form-check-label">
+                    <input class="form-check-input" type="checkbox" name="remember">Remember Me
+                    <div class="valid-feedback">Valid.</div>
+                    <div class="invalid-feedback">Check this checkbox to continue.</div>
+                </label>
+            </div>
+            <button type="submit" class="btn btn-danger">Log In</button>
+        </form>
+
+        <!-- <script>
+            // Disable form submissions if there are invalid fields
+            (function () {
+                'use strict';
+                window.addEventListener('load', function () {
+                    // Get the forms we want to add validation styles to
+                    var forms = document.getElementsByClassName('needs-validation');
+                    // Loop over them and prevent submission
+                    var validation = Array.prototype.filter.call(forms, function (form) {
+                        form.addEventListener('submit', function (event) {
+                            if (form.checkValidity() === false) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+                            form.classList.add('was-validated');
+                        }, false);
+                    });
+                }, false);
+            })();
+        </script> -->
     </template>
 </Modal>
 </template>
