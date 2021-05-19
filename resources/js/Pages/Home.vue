910b053703ae5485e465b85076651174c1cd5475
Vbuddies <cheesychase@myexcel.com>
2021-05-19 14:53:48 -0400

    added script to login form

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -183,13 +183,6 @@
             <button type="submit" class="btn btn-danger">Log In</button>
         </form>
         </div> -->
-        <script>
-            methods: {
-                async login() {
-                    console.log("log in");
-                }
-            }
-        </script>
     </template>
   </Modal>
 </template>
@@ -199,7 +192,40 @@ import Modal from "../Components/Modal";
 import FormInput from "../Components/FormInput";
 import BaseBtn from "../Components/BaseBtn";
 export default {
-    components: { Modal, FormInput, BaseBtn },
+  components: { Modal, FormInput, BaseBtn },
+  data() {
+    return {
+      email: null,
+      password: null,
+      error: null,
+    };
+  },
+  methods: {
+    async login() {
+        console.log("log in");
+      const payload = {
+        email: this.email,
+        password: this.password,
+      };
+      this.error = null;
+      try {
+        await AuthService.login(payload);
+        const authUser = await this.$store.dispatch("auth/getAuthUser");
+        if (authUser) {
+          this.$store.dispatch("auth/setGuest", { value: "isNotGuest" });
+          this.$router.push("/dashboard");
+        } else {
+          const error = Error(
+            "Unable to fetch user after login, check your API settings."
+          );
+          error.name = "Fetch User";
+          throw error;
+        }
+      } catch (error) {
+        this.error = getError(error);
+      }
+    },
+  },
 };
 </script>
 
