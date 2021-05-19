746e7738a4f6e347ca4e9d22d141e622ec14878a
maxb00 <maxabr00@yahoo.com>
2021-05-19 12:38:26 -0400

    namespace

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\Controller;
