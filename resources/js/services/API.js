63a002eddb94ba8b80b63edddca553872241ae37
maxb00 <maxabr00@yahoo.com>
2021-05-20 11:34:45 -0400

    setup login js like example

---
+++ resources/js/services/API.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import store from "@/store/index";
+
+export const apiClient = axios.create({
+  baseURL: process.env.VUE_APP_API_URL + "/api",
+  withCredentials: true, // required to handle the CSRF token
+});
+
+/*
+ * Add a response interceptor
+ */
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    if (
+      error.response &&
+      (error.response.status === 401 || error.response.status === 419)
+    ) {
+      store.dispatch("auth/logout");
+    }
+    return Promise.reject(error);
+  }
+);
