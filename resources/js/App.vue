b5e5171c25f2664c0cb6a283906def6cdbec9c07
ICAZ117 <ibraheemcazalas@gmail.com>
2021-05-20 14:20:25 -0400

    Rebound isLoginOpen and directed the data to home

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <Navbar @open-login="toggleLogin"/>
-    <router-view></router-view>
+    <router-view name="home" :isLoginOpen="isLoginOpen"></router-view>
   </div>
 </template>
 
