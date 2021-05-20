8a1737ad802cab22d94674a380b5a0c3991afddb
Vbuddies <cheesychase@myexcel.com>
2021-05-19 21:54:54 -0400

    undid changes

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -1,21 +1,20 @@
 <template>
-  <section @click="handleClose" class="modal-section">
-    <!-- <div class="absolute inset-0"> -->
-    <div class="modal-wrapper-1">
-      <div class="modal-wrapper-2">
-        <div class="modal-wrapper-3">
-          <div class="modal-wrapper-4">
-            <h3>
-              <slot name="title"></slot>
-            </h3>
-            <main>
-              <slot name="body"></slot>
-            </main>
-          </div>
+  <section @click="handleClose" class="modal-section"></section>
+  <!-- <div class="absolute inset-0"> -->
+  <div class="modal-wrapper-1">
+    <div class="modal-wrapper-2">
+      <div class="modal-wrapper-3">
+        <div class="modal-wrapper-4">
+          <h3>
+            <slot name="title"></slot>
+          </h3>
+          <main>
+            <slot name="body"></slot>
+          </main>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
