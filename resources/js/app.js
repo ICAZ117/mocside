da17c05ea7ad1f848e948114e95f0b93cbca5969
maxb00 <maxabr00@yahoo.com>
2021-05-20 12:13:31 -0400

    cleaned app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,11 @@
 require('./bootstrap');
 
 import { createApp } from 'vue';
-// import VueRouter from 'vue-router';
 
 import router from "./Router/index";
-console.log("this far")
-import App from "./App.vue";
-console.log('but not here')
 import store from "./Store/index";
+import App from "./App.vue";
+
 
 // Font Awesome
 import "./all.min";
@@ -15,13 +13,6 @@ import "./all.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-// Vue.use(VueRouter);
-
-// const app = new Vue({
-//     el: '#app',
-//     router,
-//     components: { App }
-// })
 
 const app = createApp(App);
 app.use(router);
