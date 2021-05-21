26912a6e92351826b9afcaa78950374e88840256
Vbuddies <cheesychase@myexcel.com>
2021-05-20 23:54:20 -0400

    adjusted courses file

--- resources/js/Pages/Courses.vue
+++ resources/js/Pages/Courses.vue
@@ -1,9 +1,102 @@
 <template>
-	<h1>Courses</h1>
+	<body>
+    <!-- Main Page-->
+
+    <div class="courses header">
+        <h2>My Courses</h2>
+        <hr>
+    </div>
+
+    <div class="coursecontainer">
+        <div class="courserow row my-5">
+            <!-- Image Card 1 -->
+            <a href="labs.php" class="no-decor">
+                <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                    <div class="card coursecard w-100">
+                        <div class="courses card-img-top" style="background-image: url(img/courses/1.jpg);"></div>
+
+                        <div class="courses card-content">
+                            <h6 class="card-title my-3 mx-2 mb-0">CSC 2290: Object Oriented Programming</h6>
+
+                                <hr class="courses my-0">
+
+                                <a href="labs.php" class="courselaunch text-danger mx-2 my-1 no-decor">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <!-- Image Card 2 -->
+            <a href="labs.php" class="no-decor">
+                <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                    <div class="card coursecard w-100">
+                        <div class="courses card-img-top" style="background-image: url(img/courses/2.jpg);"></div>
+
+                        <div class="courses card-content">
+                            <h6 class="card-title my-3 mx-2 mb-0">CSC 2280: Intro to Programming</h6>
+
+                                <hr class="courses hr my-0">
+
+                                <a href="labs.php" class="courselaunch text-danger mx-2 my-1 no-decor">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <!-- Image Card 3
+            <a href="labs.php" class="no-decor">
+                <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                    <div class="card coursecard w-100">
+                        <div class="courses card-img-top" style="background-image: url(img/courses/2.jpg);"></div>
+
+                        <div class="courses card-content">
+                            <h6 class="card-title my-3 mx-2 mb-0">CSC 2280: Intro to Programming</h4>
+
+                                <hr class="courses hr my-0">
+
+                                <a href="labs.php" class="courselaunch text-danger mx-2 my-1 no-decor">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="labs.php" class="no-decor">
+                <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                    <div class="card coursecard w-100">
+                        <div class="courses card-img-top" style="background-image: url(img/courses/2.jpg);"></div>
+
+                        <div class="courses card-content">
+                            <h6 class="card-title my-3 mx-2 mb-0">CSC 2280: Intro to Programming</h4>
+
+                                <hr class="courses hr my-0">
+
+                                <a href="labs.php" class="courselaunch text-danger mx-2 my-1 no-decor">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="labs.php" class="no-decor">
+                <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                    <div class="card coursecard w-100">
+                        <div class="courses card-img-top" style="background-image: url(img/courses/2.jpg);"></div>
+
+                        <div class="courses card-content">
+                            <h6 class="card-title my-3 mx-2 mb-0">CSC 2280: Intro to Programming</h4>
+
+                                <hr class="courses hr my-0">
+
+                                <a href="labs.php" class="courselaunch text-danger mx-2 my-1 no-decor">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </a> -->
+        </div>
+    </div>
+
+</body>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
