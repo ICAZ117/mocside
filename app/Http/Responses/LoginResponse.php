2a3a27ac3c356c94adcedb0200d451dc96d5e4a8
maxb00 <maxabr00@yahoo.com>
2021-05-21 13:09:08 -0400

    created custom login response

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginResponse implements LoginResponseContract
+{
+    public function toResponse($request): Response
+    {
+        return response()->json(auth()->user());
+    }
+}
--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
 
+use App\Http\Responses\LoginResponse;
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
@@ -43,5 +46,7 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+        $this->app->singleton(LoginResponseContract::class, LoginResponse::class);
     }
 }
