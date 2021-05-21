60477936e2336c444c0de9a0137d751267fd76a1
ICAZ117 <ibraheemcazalas@gmail.com>
2021-05-21 12:37:21 -0400

    Removed old Vuelidate code from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,7 @@ import router from "./Router/index";
 // import store from "./Store/index";
 import App from "../js/App.vue";
 
-// Vuelidate
-import Vuelidate from "vuelidate";
+
 
 // Font Awesome
 import "./all.min";
@@ -17,7 +16,7 @@ import "bootstrap";
 
 
 const app = createApp(App);
-app.use(Vuelidate);
+
 app.use(router);
 // app.use(store);
 app.mount('#app');
