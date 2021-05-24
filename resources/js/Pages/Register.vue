784a0311c23367645201014f706e4bd90d62d228
Vbuddies <cheesychase@myexcel.com>
2021-05-24 15:27:27 -0400

    returned to earlier version to check if still work

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -207,7 +207,7 @@ export default {
       },
       confirmPassword: {
         required,
-        sameAsPassword: sameAs(this.userForm.password), 
+        sameAsPassword: sameAs("password"), 
       },
     },
   },
