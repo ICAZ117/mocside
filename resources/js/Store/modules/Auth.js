4187b88746cbd0160818b7a34ac08c4fef1d48fa
maxb00 <maxabr00@yahoo.com>
2021-05-24 11:23:47 -0400

    changed auth login

--- resources/js/Store/modules/Auth.js
+++ resources/js/Store/modules/Auth.js
@@ -36,22 +36,18 @@ export const actions = {
         commit("SET_ERROR", getError(error));
       });
   },
-  getAuthUser({ commit }) {
+  async getAuthUser({ commit }) {
     commit("SET_LOADING", true);
-    return AuthService.getAuthUser()
-      .then((response) => {
-        console.log("Found User.");
-        commit("SET_USER", response.data.data);
-        commit("SET_LOADING", false);
-        console.log(state);
-      })
-      .catch((error) => {
-        console.log("Couldn't find user. Unauthenticated?")
-        console.log(error);
-        commit("SET_LOADING", false);
-        commit("SET_USER", null);
-        commit("SET_ERROR", getError(error));
-      });
+    try {
+      const response = await AuthService.getAuthUser();
+      commit("SET_USER", response.data.data);
+      commit("SET_LOADING", false);
+      return response.data.data;
+    } catch (error) {
+      commit("SET_LOADING", false);
+      commit("SET_USER", null);
+      commit("SET_ERROR", getError(error));
+    }
   },
   setGuest(context, { value }) {
     window.localStorage.setItem("guest", value);
