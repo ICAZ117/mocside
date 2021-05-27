7e8c7fa3e51b9851e7b0441599ac16320c08ddf1
Vbuddies <cheesychase@myexcel.com>
2021-05-27 11:17:41 -0400

    removed print lines

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -27,16 +27,10 @@ export default {
   computed: {
     authUser: function() {
       var flag = store.getters["auth/isGuest"];
-      var flag2 = store.getters["auth/guest"];
-      console.log("flag(auth/iGuest): " + flag);
-      console.log("flag2(auth/guest): " + flag2);
       if(flag || flag == null) {
-        // this.authUser = null;
-        console.log("authUser null");
         return null;
       }
       else {
-        console.log("authUser set to User");
         return this.getAuthUser();
       }
     }  
