79d8ededf42446af76644cd19dbd1b8af476dfc1
Vbuddies <cheesychase@myexcel.com>
2021-05-27 13:45:57 -0400

    readded router-view for tests

--- resources/js/Pages/Courses.vue
+++ resources/js/Pages/Courses.vue
@@ -34,25 +34,9 @@
         <div v-if="courses.length == 0">
           <h1>No Registered Courses</h1>
         </div>
-
-        <!-- Image Card 1 -->
-        <!-- <a href="labs.php" class="no-decor">
-                <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                    <div class="card coursecard w-100">
-                        <div class="courses card-img-top" style="background-image: url(img/courses/2290.png);"></div>
-
-                        <div class="courses card-content">
-                            <h6 class="card-title my-3 mx-2 mb-0">CSC 2290: Object Oriented Programming</h6>
-
-                                <hr class="courses my-0">
-
-                                <a href="labs.php" class="courselaunch text-danger mx-2 my-1 no-decor">Get Started</a>
-                        </div>
-                    </div>
-                </div>
-            </a> -->
       </div>
     </div>
+    <router-view></router-view>
   </div>
 </template>
 
