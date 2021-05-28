52ec044580b396899a38503e5da0cdba36cb7816
maxb00 <maxabr00@yahoo.com>
2021-05-28 12:06:06 -0400

    Fixed import

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Assignment;
+use App\Models\Lab;
 use App\Http\Resources\AssignmentResource;
 
 class AssignmentController extends Controller
