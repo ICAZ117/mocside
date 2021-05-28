9741a87b56d6f8cb447729eef0a77de5b028ac77
maxb00 <maxabr00@yahoo.com>
2021-05-28 13:33:47 -0400

    Created Test Case Resource

---
+++ app/Models/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TestCase extends Model
+{
+    use HasFactory;
+    protected $table = 'test_cases';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id',
+        'assignment_id',
+        'input',
+        'output'
+    ];
+
+    public function assignment()
+    {
+        return $this->belongsTo(Assignment::class);
+    }
+}
