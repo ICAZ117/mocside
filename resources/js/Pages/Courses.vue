fe8fef19dce264a6f25309e2ae74cbc65d126434
Vbuddies <cheesychase@myexcel.com>
2021-05-31 13:14:28 -0400

    attempting styling

--- resources/js/Pages/Courses.vue
+++ resources/js/Pages/Courses.vue
@@ -10,8 +10,8 @@
       <div class="coursecontainer">
         <div class="courserow row my-5">
           <div v-for="course in courses" :key="course.id">
-            <a @click="goToLabs(course.id)"  class="no-decor">
-                <!-- :to="{ name: 'Labs', params: { id: course.id } }" -->
+            <a @click="goToLabs(course.id)" class="no-decor">
+              <!-- :to="{ name: 'Labs', params: { id: course.id } }" -->
               <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
                 <div class="card coursecard w-100">
                   <div
@@ -26,12 +26,12 @@
                     <!-- <a href="Labs.vue" class="courselaunch text-danger mx-2 my-1 no-decor"
                       >Get Started</a
                     > -->
-                    <a @click="editCourse(course.id)" class="courselaunch text-danger mx-2 my-1 no-decor">•••</a>
-                    <a @click="deleteCourse(course.id)" class="courselaunch text-danger mx-2 my-1 no-decor">X</a>
                   </div>
                 </div>
               </div>
             </a>
+            <a @click="editCourse(course.id)" class="courselaunch text-danger mx-2 my-1 no-decor">•••</a>
+            <a @click="deleteCourse(course.id)" class="courselaunch text-danger mx-2 my-1 no-decor">X</a>
           </div>
           <div class="add-course">
             <a @click="addCourse()" class="no-decor">
@@ -39,7 +39,9 @@
                 <div class="card coursecard w-100">
                   <div
                     class="courses card-img-top"
-                    :style="{ backgroundImage: `url('../../img/courses/addcourse.png')` }"
+                    :style="{
+                      backgroundImage: `url('../../img/courses/addcourse.png')`,
+                    }"
                   ></div>
                   <!-- <div class="courses card-content">
                     <h6 class="card-title my-3 mx-2 mb-0">Add Course</h6>
@@ -58,7 +60,12 @@
         </div>
       </div>
     </div>
-    <router-view @lab-unmounting="childUnmounting()" @edit-unmounting="childUnmounting()" v-if="childIsOpen" :courseID="courseID"></router-view>
+    <router-view
+      @lab-unmounting="childUnmounting()"
+      @edit-unmounting="childUnmounting()"
+      v-if="childIsOpen"
+      :courseID="courseID"
+    ></router-view>
   </div>
 </template>
 
@@ -80,16 +87,22 @@ export default {
       var payload = {
         name: "New Course",
         description: "New Course",
-      }
+      };
       const course = await API.apiClient.post(`/courses`, payload);
       this.courseID = course.data.id;
       this.childIsOpen = true;
-      this.$router.push({ name: 'EditCourse', params: { course_id: this.courseID } })
+      this.$router.push({
+        name: "EditCourse",
+        params: { course_id: this.courseID },
+      });
     },
     editCourse(id) {
       this.childIsOpen = true;
       this.CourseID = id;
-      this.$router.push({ name: 'EditCourse', params: { course_id: this.courseID } })
+      this.$router.push({
+        name: "EditCourse",
+        params: { course_id: this.courseID },
+      });
     },
     deleteCourse(id) {
       this.childIsOpen = false;
@@ -100,7 +113,7 @@ export default {
     goToLabs(id) {
       this.childIsOpen = true;
       this.courseID = id;
-      this.$router.push({ name: 'Labs', params: { course_id: this.courseID } })
+      this.$router.push({ name: "Labs", params: { course_id: this.courseID } });
     },
     async getCourses() {
       var i;
@@ -111,15 +124,15 @@ export default {
       }
     },
     childUnmounting() {
-      this.childIsOpen=false;
-      this.courseID=null;
+      this.childIsOpen = false;
+      this.courseID = null;
     },
   },
   mounted() {
     this.childIsOpen = false;
     this.authUser = store.getters["auth/authUser"];
-    if (this.authUser.courses){
-        this.enrolledCourses = JSON.parse(this.authUser.courses).courses;
+    if (this.authUser.courses) {
+      this.enrolledCourses = JSON.parse(this.authUser.courses).courses;
     }
     this.getCourses();
   },
