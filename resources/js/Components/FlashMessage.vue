5ed15a3d5ea7993c24069080fecdb61050b32360
Vbuddies <cheesychase@myexcel.com>
2021-06-01 13:05:59 -0400

    added fileupload and flashmessage vue files

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p v-if="message" class="mt-2 text-sm text-green-500">
+      {{ message }}
+    </p>
+    <p v-if="error" class="mt-2 text-sm text-red-500">
+      {{ error }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlashMessage",
+  props: {
+    message: {
+      type: String,
+      default: null,
+    },
+    error: {
+      type: [Error, String],
+      default: null,
+    },
+  },
+};
+</script>
