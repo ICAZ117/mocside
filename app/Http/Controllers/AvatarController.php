0776f09edce841185f1d42533a5a4f9ab4506ad0
maxb00 <maxabr00@yahoo.com>
2021-06-01 14:03:04 -0400

    updated return to asset link

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -11,8 +11,8 @@ class AvatarController extends Controller
     public function store(Request $request)
     {
         try {
-            $filePath = Storage::disk('local')->putFile('images', $request->file);
-            return response()->json(['message' => $filePath], 200);
+            $filePath = Storage::disk('local')->putFile('public/images', $request->file);
+            return response()->json(['message' => asset('storage/'.$filePath)], 200);
         } catch (Exception $exception) {
             return response()->json(['message' => $exception->getMessage()], 409);
         }
