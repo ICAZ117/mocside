c804bfafa18cefaa45ef486de577dbf673117929
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-01 13:45:03 -0400

    Message (Ctrl + Enter to commit on 'main')

--- resources/js/Pages/Workspace.vue
+++ resources/js/Pages/Workspace.vue
@@ -38,56 +38,58 @@
           </button>
           <div style="width: 78% !important">
             <div style="float: right !important">
-              <label for="theme" class="workspace-label">Theme: </label>
-              <select v-model="theme" id="theme" class="console-select form-select">
-                <optgroup label="Dark">
-                  <option value="ambiance">Ambiance</option>
-                  <option value="chaos">Chaos</option>
-                  <option value="clouds_midnight">Clouds Midnight</option>
-                  <option value="dracula">Dracula</option>
-                  <option value="cobalt">Cobalt</option>
-                  <option value="gruvbox">Gruvbox</option>
-                  <option value="gob" selected>Green on Black</option>
-                  <option value="idle_fingers">idle Fingers</option>
-                  <option value="kr_theme">krTheme</option>
-                  <option value="merbivore">Merbivore</option>
-                  <option value="merbivore_soft">Merbivore Soft</option>
-                  <option value="mono_industrial">Mono Industrial</option>
-                  <option value="monokai">Monokai</option>
-                  <option value="nord_dark">Nord Dark</option>
-                  <option value="pastel_on_dark">Pastel on dark</option>
-                  <option value="solarized_dark">Solarized Dark</option>
-                  <option value="terminal">Terminal</option>
-                  <option value="tomorrow_night">Tomorrow Night</option>
-                  <option value="tomorrow_night_blue">Tomorrow Night Blue</option>
-                  <option value="tomorrow_night_bright">Tomorrow Night Bright</option>
-                  <option value="tomorrow_night_eighties">Tomorrow Night 80s</option>
-                  <option value="twilight">Twilight</option>
-                  <option value="vibrant_ink">Vibrant Ink</option>
-                </optgroup>
-                <optgroup label="Light">
-                  <option value="chrome">Chrome</option>
-                  <option value="clouds">Clouds</option>
-                  <option value="crimson_editor">Crimson Editor</option>
-                  <option value="dawn">Dawn</option>
-                  <option value="dreamweaver">Dreamweaver</option>
-                  <option value="eclipse">Eclipse</option>
-                  <option value="github">GitHub</option>
-                  <option value="iplastic">IPlastic</option>
-                  <option value="solarized_light">Solarized Light</option>
-                  <option value="textmate">TextMate</option>
-                  <option value="tomorrow">Tomorrow</option>
-                  <option value="xcode">Xcode</option>
-                  <option value="kuroir">Kuroir</option>
-                  <option value="katzenmilch">KatzenMilch</option>
-                  <option value="sqlserver">SQL Server</option>
-                </optgroup>
-              </select>
-              <label for="lang" class="workspace-label">Language: </label>
-              <select v-model="lang" id="lang" class="console-select form-select">
-                <option value="java" selected>Java</option>
-                <option value="python">Python</option>
-              </select>
+              <div style="display: block !important">
+                <label for="theme" class="workspace-label">Theme: </label>
+                <select v-model="theme" id="theme" class="console-select form-select">
+                  <optgroup label="Dark">
+                    <option value="ambiance">Ambiance</option>
+                    <option value="chaos">Chaos</option>
+                    <option value="clouds_midnight">Clouds Midnight</option>
+                    <option value="dracula">Dracula</option>
+                    <option value="cobalt">Cobalt</option>
+                    <option value="gruvbox">Gruvbox</option>
+                    <option value="gob" selected>Green on Black</option>
+                    <option value="idle_fingers">idle Fingers</option>
+                    <option value="kr_theme">krTheme</option>
+                    <option value="merbivore">Merbivore</option>
+                    <option value="merbivore_soft">Merbivore Soft</option>
+                    <option value="mono_industrial">Mono Industrial</option>
+                    <option value="monokai">Monokai</option>
+                    <option value="nord_dark">Nord Dark</option>
+                    <option value="pastel_on_dark">Pastel on dark</option>
+                    <option value="solarized_dark">Solarized Dark</option>
+                    <option value="terminal">Terminal</option>
+                    <option value="tomorrow_night">Tomorrow Night</option>
+                    <option value="tomorrow_night_blue">Tomorrow Night Blue</option>
+                    <option value="tomorrow_night_bright">Tomorrow Night Bright</option>
+                    <option value="tomorrow_night_eighties">Tomorrow Night 80s</option>
+                    <option value="twilight">Twilight</option>
+                    <option value="vibrant_ink">Vibrant Ink</option>
+                  </optgroup>
+                  <optgroup label="Light">
+                    <option value="chrome">Chrome</option>
+                    <option value="clouds">Clouds</option>
+                    <option value="crimson_editor">Crimson Editor</option>
+                    <option value="dawn">Dawn</option>
+                    <option value="dreamweaver">Dreamweaver</option>
+                    <option value="eclipse">Eclipse</option>
+                    <option value="github">GitHub</option>
+                    <option value="iplastic">IPlastic</option>
+                    <option value="solarized_light">Solarized Light</option>
+                    <option value="textmate">TextMate</option>
+                    <option value="tomorrow">Tomorrow</option>
+                    <option value="xcode">Xcode</option>
+                    <option value="kuroir">Kuroir</option>
+                    <option value="katzenmilch">KatzenMilch</option>
+                    <option value="sqlserver">SQL Server</option>
+                  </optgroup>
+                </select>
+                <label for="lang" class="workspace-label">Language: </label>
+                <select v-model="lang" id="lang" class="console-select form-select">
+                  <option value="java" selected>Java</option>
+                  <option value="python">Python</option>
+                </select>
+              </div>
             </div>
           </div>
         </div>
