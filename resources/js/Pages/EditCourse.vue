de533817b444acd5f9c6157a46626667b9f73e7e
Vbuddies <cheesychase@myexcel.com>
2021-06-02 12:11:34 -0400

    added await

--- resources/js/Pages/EditCourse.vue
+++ resources/js/Pages/EditCourse.vue
@@ -151,7 +151,7 @@ export default {
   methods: {
     async handleSubmit() {
       this.isSubmitted = true;
-      this.uploadImage();
+      await this.uploadImage();
       var payload = {
         name: this.courseForm.name,
         description: this.courseForm.description,
