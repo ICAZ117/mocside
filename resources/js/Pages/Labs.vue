8c2e51710d19817b98298c4a4e789f5154219d69
Vbuddies <cheesychase@myexcel.com>
2021-06-03 14:52:46 -0400

    fixed import

--- resources/js/Pages/Labs.vue
+++ resources/js/Pages/Labs.vue
@@ -51,7 +51,7 @@
 
 <script>
 import * as API from "../services/API";
-import store from "./Store/index";
+import store from "../Store/index";
 export default {
   props: ["courseID"],
   emits: ["unmounting"],
