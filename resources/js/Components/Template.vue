afa7496f2be19a91583f779d04be5cdca79a8836
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-04 13:00:53 -0400

    Created editor-launch for template

--- resources/js/Components/Template.vue
+++ resources/js/Components/Template.vue
@@ -1,17 +1,39 @@
 <template>
-  <div class="create-assignment">
-    <div class="template-workspace">
+  <div class="create-assignment" style="padding: 0 !important">
+    <div class="lang-select center">
+      <div class="row">
+        <div class="col-9">
+          <select v-model="lang" id="lang" class="form-select">
+            <option value="" selected disabled hidden>Select a language...</option>
+            <option value="Java">Java</option>
+            <option value="Python">Python</option>
+          </select>
+        </div>
+        <div class="col-3 ml-1">
+          <button
+            type="launch"
+            name="launch"
+            class="launch-workspace btn btn-success"
+            :disabled="!lang.length"
+            @click="launchEditor()"
+          >
+            Launch in {{ lang }}
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="!lang.length" class="template-workspace">
       <div class="save-template">
         <div class="row">
           <button class="btn btn-primary btn-md col-4" @click="saveTemplate()">
             SAVE CHANGES
           </button>
-          <small class="col-8">{{ saveStatus }}</small>
+          <small class="col-8 saveStatus">{{ saveStatus }}</small>
         </div>
       </div>
 
-      <IDE :lang="lang" :showSubmit="false"/>
-
+      <IDE :lang="lang" :showSubmit="false" />
     </div>
   </div>
 </template>
@@ -21,10 +43,13 @@ export default {
   data() {
     return {
       saveStatus: "All changes have been saved",
-      lang: "Java",
+      lang: "",
     };
   },
   methods: {
+    launchEditor() {
+      this.$forceUpdate();
+    },
     saveTemplate() {
       console.log("Inside saveTemplate()");
     },
