7064cf0bd081313db2251741700669fa959ba193
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-04 14:04:34 -0400

    Testing basic bootstrap card

--- resources/js/Components/TestBench.vue
+++ resources/js/Components/TestBench.vue
@@ -1,13 +1,25 @@
 <template>
-  Test Bench
+  <div class="create-assignment">
+    <div class="card">
+      <div class="card-title">
+        <p>tc1</p>
+      </div>
+
+      <hr>
+
+      <div class="card-body">
+        <p>
+          Input/Output Comparison
+          <br />
+          100 Points
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
