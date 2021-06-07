c0ea3a6063b6774185076d785af298ab9ebd6b41
maxb00 <maxabr00@yahoo.com>
2021-06-07 12:35:52 -0400

    Updated assignment controller

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -26,12 +26,6 @@ class Course extends Model
     // protected $casts = [
     //     'published' => 'boolean',
     // ];
-    
-    // This is commented because I don't understand eloquest relationships :)
-    // each course should be owned by a single user, who will have perm to delete/edit it.
-    // public function owner() {
-    //     return $this->belongsTo('user', 'user_id');
-    // }
 
     public function owner()
     {
