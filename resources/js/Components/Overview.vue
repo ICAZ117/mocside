19ce2a3d0344ddb191e366cf70a6105d87f77878
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-07 12:30:54 -0400

    Testing description

--- resources/js/Components/Overview.vue
+++ resources/js/Components/Overview.vue
@@ -2,7 +2,7 @@
   <div class="create-assignment">
     <div class="container">
       <h4>Description:</h4>
-      <QuillEditor theme="snow" toolbar="full" class="assignment-description" v-model:value="text"/>
+      <QuillEditor theme="snow" toolbar="full" class="assignment-description" v-model:value="description"/>
       <hr />
       <h5>Proceed with caution!</h5>
       <button class="btn btn-danger btn-lg">DELETE ASSIGNMENT</button>
