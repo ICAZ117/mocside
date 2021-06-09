30b25c0c3a6de9c02f84b21029432fa0cef3acf9
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-09 11:26:18 -0400

    Testing navbar

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -64,6 +64,8 @@ export default {
     console.log(window.location.pathname);
     console.log("\n\nthis.$router.currentRoute.path:");
     console.log(this.$router.currentRoute.path);
+    console.log("\n\ndocument.location:");
+    console.log(document.location);
   },
 };
 </script>
