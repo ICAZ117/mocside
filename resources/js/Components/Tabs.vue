9bd16be16fec4c1d6558393f5d0e5a42ba8fe6f0
Vbuddies <cheesychase@myexcel.com>
2021-06-09 13:51:34 -0400

    creating a custom tab component

---
+++ resources/js/Components/Tabs.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+	  <ul class='tabs__header'>
+		  <li v-for="tab in tabs" :key='tab.title'>
+			  {{ tab.title }}
+		  </li>
+	  </ul>
+	  <slot></slot>
+  </div>
+</template>
+
+<script>
+import Tab from "./Tab.vue";
+export default {
+	data() {
+		return {
+			selectedIndex: 0,
+			tabs: [],
+		}
+	},
+	created() {
+		this.tabs = this.$children;
+	}
+
+}
+</script>
+
+<style>
+
+</style>
