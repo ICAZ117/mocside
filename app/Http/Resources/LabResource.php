add4221ba754a0b1df1205cadaee008a8250dfcc
maxb00 <maxabr00@yahoo.com>
2021-06-09 11:50:58 -0400

    added total points count to Lab resource

--- app/Http/Resources/LabResource.php
+++ app/Http/Resources/LabResource.php
@@ -14,6 +14,13 @@ class LabResource extends JsonResource
      */
     public function toArray($request)
     {
+        $assignments = $this->assignments;
+        $total = 0;
+        for ($i = 0; $i < count($assignments); $i++) {
+          $points = $assignments[$i]->test_cases->count();
+          $total = $total + $points;
+        }
+
         return [
           'id' => $this->id,
           'name' => $this->name,
@@ -21,6 +28,7 @@ class LabResource extends JsonResource
           'num_problems' => $this->assignments->count(),
           'due_date' => $this->due_date,
           'publish_date' => $this->publish_date,
+          'total_points' => $total,
         ];
     }
 }
