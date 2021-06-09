e85ec8ff6b10fdc2e4db7627b969e370fc102051
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-09 12:09:01 -0400

    Debugging

--- resources/js/Components/Overview.vue
+++ resources/js/Components/Overview.vue
@@ -28,13 +28,14 @@ export default {
   props: ["overview", "problemID"],
   setup() {
     const state = reactive({
-      content: {
-        ops: [
-          {
-            insert: "Gandalf the Grey\n",
-          },
-        ],
-      },
+      // content: {
+      //   ops: [
+      //     {
+      //       insert: "Gandalf the Grey\n",
+      //     },
+      //   ],
+      // },
+      content: "",
     });
 
     return { state };
@@ -65,7 +66,23 @@ export default {
     },
   },
   mounted() {
+    console.log("\n\n----------------------- Before -----------------------");
+    console.log("this.overview");
+    console.log(this.overview);
+    console.log("json parse this.overview");
+    console.log(JSON.parse(this.overview));
+    console.log("\n\nthis.state.content");
+    console.log(this.state.content);
+
     this.state.content = this.overview;
+
+    console.log("\n\n----------------------- After -----------------------");
+    console.log("this.overview");
+    console.log(this.overview);
+    console.log("json parse this.overview");
+    console.log(JSON.parse(this.overview));
+    console.log("\n\nthis.state.content");
+    console.log(this.state.content);
   },
 };
 </script>
