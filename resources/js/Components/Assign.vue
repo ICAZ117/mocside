73f9ba0914cc4b6a2b31ba4801d7207ac981971f
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-10 15:52:23 -0400

    Fixed some styling issues for  assign coursecards

--- resources/js/Components/Assign.vue
+++ resources/js/Components/Assign.vue
@@ -4,56 +4,62 @@
       <button class="btn btn-primary btn-md publish-all">PUBLISH TO ALL</button>
       <br />
 
-      <div v-for="course in courses" :key="course.id">
-        <div class="no-decor">
-          <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-            <div class="card editcoursecard w-100">
-              <div class="courses card-content">
-                <h4 class="card-title my-3 mx-2 mb-0">
-                  {{ course.name }}
-                </h4>
+      <div class="row">
+        <div v-for="course in courses" :key="course.id">
+          <div class="no-decor">
+            <div class="width col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
+              <div class="card editcoursecard w-100">
+                <div class="courses card-content">
+                  <h4 class="card-title my-3 mx-2 mb-0">
+                    {{ course.name }}
+                  </h4>
 
-                <hr class="courses my-0" />
+                  <hr class="courses my-0" />
 
-                <div class="card-body center">
-                  <div class="row">
-                    <div class="col-4">Publish:</div>
-                    <div class="col-8">
-                      <label class="switch">
-                        <input type="checkbox" v-model="publish" />
-                        <span class="slider round"></span>
-                      </label>
+                  <div class="card-body center">
+                    <div class="row">
+                      <div class="col-4">Publish:</div>
+                      <div class="col-8">
+                        <label class="switch">
+                          <input type="checkbox" v-model="publish" />
+                          <span class="slider round"></span>
+                        </label>
+                      </div>
                     </div>
+
+                    <br />
+
+                    <label for="lab-select">Lab:</label>
+                    <br />
+                    <small>
+                      <select id="lab-select">
+                        <option value="" selected hidden disabled>Select a lab...</option>
+                        <option v-for="lab in course.labs" :key="lab.id" :value="lab.id">
+                          {{ lab.name }}
+                        </option>
+                      </select>
+                    </small>
+
+                    <br />
+
+                    <label for="dueDate">Due Date: </label>
+                    <input type="date" id="dueDate" v-model="dueDate" />
                   </div>
 
-                  <br />
+                  <hr class="courses my-0" />
 
-                  <small>
-                    <select id="labs">
-                      <option value="" selected hidden disabled>Select a lab...</option>
-                      <option v-for="lab in course.labs" :key="lab.id" :value="lab.id">
-                        {{ lab.name }}
-                      </option>
-                    </select>
-                  </small>
-
-                  <label for="dueDate">Due Date: </label>
-                  <input type="date" id="dueDate" v-model="dueDate" />
-                </div>
-
-                <hr class="courses my-0" />
-
-                <div class="card-footer">
-                  <div class="row center">
-                    <div class="col-6">
-                      <small><a href="" class="no-decor">Student Preview</a></small>
-                    </div>
-                    <div class="col-6">
-                      <small
-                        ><a href="" class="no-decor"
-                          >Gradebook &<br />Submissions</a
-                        ></small
-                      >
+                  <div class="card-footer">
+                    <div class="row center">
+                      <div class="col-6">
+                        <small><a href="" class="no-decor">Student Preview</a></small>
+                      </div>
+                      <div class="col-6">
+                        <small
+                          ><a href="" class="no-decor"
+                            >Gradebook &<br />Submissions</a
+                          ></small
+                        >
+                      </div>
                     </div>
                   </div>
                 </div>
