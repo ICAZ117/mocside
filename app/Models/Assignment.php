10ad7106d43c34902762cce65f2079dbec196ab7
maxb00 <maxabr00@yahoo.com>
2021-06-10 16:40:19 -0400

    Added smart delete and update with copy_id

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -18,7 +18,6 @@ class Assignment extends Model
         'python_model',
         'java_starter',
         'java_model',
-        'test_cases',
         'copy_id',
         // v-- these are specific across copied instances.
         'due_date',
@@ -34,5 +33,15 @@ class Assignment extends Model
     public function test_cases()
     {
         return $this->hasMany(TestCase::class);
+    }
+
+    public function worth()
+    {
+        $test_cases = $this->test_cases;
+        $total = 0;
+        for ($i = 0; $i < count($test_cases); $i++) {
+            $total = $total + $test_cases[$i]->points;
+        }
+        return $total;
     }
 }
