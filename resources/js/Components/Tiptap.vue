38ea1987cf7042de0adbd0802084ec7aaa93b55c
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-11 12:51:21 -0400

    Removed typography extension

--- resources/js/Components/Tiptap.vue
+++ resources/js/Components/Tiptap.vue
@@ -110,7 +110,6 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import Code from '@tiptap/extension-code'
-import Typography from '@tiptap/extension-typography'
 import { ColorHighlighter } from './ColorHighlighter'
 import { SmilieReplacer } from './SmilieReplacer'
 
@@ -130,7 +129,6 @@ export default {
         Paragraph,
         Text,
         Code,
-        Typography,
         ColorHighlighter,
         SmilieReplacer,
       ],
