6c3882f554e20fd0e7dca48484fb9a26826c752c
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-11 12:47:02 -0400

    Added a bunch of extensions

--- resources/js/Components/Tiptap.vue
+++ resources/js/Components/Tiptap.vue
@@ -105,60 +105,42 @@
 </template>
 
 <script>
-import { Editor, EditorContent } from "@tiptap/vue-3";
-import StarterKit from "@tiptap/starter-kit";
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Code from '@tiptap/extension-code'
+import Typography from '@tiptap/extension-typography'
+import { ColorHighlighter } from './ColorHighlighter'
+import { SmilieReplacer } from './SmilieReplacer'
 
 export default {
   components: {
     EditorContent,
   },
-
   data() {
     return {
       editor: null,
-    };
+    }
   },
-
   mounted() {
     this.editor = new Editor({
-      extensions: [StarterKit],
-      content: `
-        <h2>
-          Hi there,
-        </h2>
-        <p>
-          this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-        </p>
-        <ul>
-          <li>
-            That’s a bullet list with one …
-          </li>
-          <li>
-            … or two list items.
-          </li>
-        </ul>
-        <p>
-          Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-        </p>
-        <pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-        <p>
-          I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-        </p>
-        <blockquote>
-          Wow, that’s amazing. Good work, boy! 👏
-          <br />
-          — Mom
-        </blockquote>
-      `,
-    });
+      extensions: [
+        Document,
+        Paragraph,
+        Text,
+        Code,
+        Typography,
+        ColorHighlighter,
+        SmilieReplacer,
+      ],
+      content: "This is cool!",
+    })
   },
-
   beforeDestroy() {
-    this.editor.destroy();
+    this.editor.destroy()
   },
-};
+}
 </script>
 
 <style>
