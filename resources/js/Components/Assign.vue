eeec2a1853df979c139df4b1af1216313a14b024
Vbuddies <cheesychase@myexcel.com>
2021-06-11 14:12:55 -0400

    testing

--- resources/js/Components/Assign.vue
+++ resources/js/Components/Assign.vue
@@ -116,7 +116,8 @@ export default {
     },
     async splitCourses() {
       const res = await API.apiClient.get(`/problems/copies/${this.problemID}`);
-      var raw = JSON.parse(res.data.data);
+      console.log(res);
+      var raw = JSON.parse(res.data);
       console.log(raw);
       var tempList = [];
       for(let i = 0; i < raw.length; i++) {
