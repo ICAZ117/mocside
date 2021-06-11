760c5ffe6bba9e37c74d28f82f5e33fed170ad2f
Vbuddies <cheesychase@myexcel.com>
2021-06-11 12:17:21 -0400

    added tiptap to about

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,8 +1,12 @@
 <template>
-  
+  <Tiptap />
 </template>
 
 <script>
-  import Tiptap from "../Components/TipTap"
-  export default {}
+import Tiptap from "../Components/TipTap.vue";
+export default {
+  components: {
+    Tiptap,
+  },
+};
 </script>
