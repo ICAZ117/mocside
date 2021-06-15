a0c8480ea3170c69b68e909ef4da905373474f81
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-15 12:52:06 -0400

    Debugging

--- resources/js/Components/Overview.vue
+++ resources/js/Components/Overview.vue
@@ -2,7 +2,7 @@
   <div class="create-assignment">
     <div class="container">
       <h4>Description:</h4>
-      <Tiptap :savedText="savedText" @input="save" />
+      <Tiptap :savedText="JSON.parse(overview)" @input="save" />
       <hr />
       <h5>Proceed with caution!</h5>
       <button class="btn btn-danger btn-lg">DELETE ASSIGNMENT</button>
@@ -17,7 +17,7 @@ import * as API from "../services/API";
 export default {
   props: {
     overview: {
-      type: Object,
+      type: String,
       required: true,
     },
     problemID: {
@@ -27,7 +27,7 @@ export default {
   },
   data() {
     return {
-      savedText: this.overview,
+      savedText: JSON.parse(this.overview),
       assignmentID: this.problemID,
       newText: {},
     };
@@ -45,7 +45,11 @@ export default {
     },
   },
   beforeMount() {
-    console.log("HERE");
+    console.log("before");
+    console.log(this.savedText);
+  },
+  mounted() {
+    console.log("After");
     console.log(this.savedText);
   },
 };
