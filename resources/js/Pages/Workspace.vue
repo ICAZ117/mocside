e56776e35478197f7d6de77cd082cfc5596ff50c
Vbuddies <cheesychase@myexcel.com>
2021-06-16 13:37:29 -0400

    adding grabbing of problem from database

--- resources/js/Pages/Workspace.vue
+++ resources/js/Pages/Workspace.vue
@@ -24,12 +24,21 @@
 </template>
 
 <script>
+import * as API from "../services/API";
 export default {
   props: ["problemID", "lang"],
   emits: ["unmounting"],
   data() {
     return {
       assignment: {},
+      workspace: {
+        title: "",
+        description: "",
+        code_j: {},
+        code_p: {},
+        theme: "",
+        input: "",
+      },
     };
   },
   methods: {
@@ -37,10 +46,26 @@ export default {
       //this route needs to be worked on and adjusted
       const rawAssignment = await API.apiClient.get(`/problems/${this.labID}`);
       this.assignment = rawAssignment.data;
+      this.workspace.title = this.assignment.name;
+      this.workspace.description = this.assignment.description;
+      const res = await API.apiClient.get(`/problems/full/${this.problemID}`);
+      this.workspace.code_j = await getJava(res.data.data);
+      this.workspace.code_p = await getPython(res.data.data);
+    },
+    async getJava(templates) {
+      //if first time opening grab template, else grab student code
+      return templates.java_starter;
+    },
+    async getPython(templates) {
+      //if first time opening grab template, else grab student code
+      return templates.python_starter;
     },
   },
   beforeUnmount() {
     this.$emit("unmounting");
+  },
+  mount() {
+    this.getAssignment();
   },
 };
 </script>
