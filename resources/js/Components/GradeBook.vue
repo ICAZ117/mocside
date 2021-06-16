999bfc1a07e45cb3893bbb8e101a5387016b4e18
maxb00 <maxabr00@yahoo.com>
2021-06-16 11:30:11 -0400

    log

--- resources/js/Components/GradeBook.vue
+++ resources/js/Components/GradeBook.vue
@@ -65,7 +65,7 @@ export default {
 
         // this logic is populating front-end gradebook
         console.log(this.gradebook);
-        console.log(this.gradebook["students"]);
+        console.log(this.gradebook.students);
         var student_ids = this.gradebook.students; // list of ids in gradebook 
         var curr;
         for (let i = 0; i < student_ids.length; i++) {
