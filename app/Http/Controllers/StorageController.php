5f3b6bcdfc5efb0b7fd613709f68d3d58516fc39
maxb00 <maxabr00@yahoo.com>
2021-06-17 12:03:35 -0400

    storage :(

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\File;
 use App\Models\Code;
 
 class StorageController extends Controller
@@ -51,7 +52,7 @@ class StorageController extends Controller
             $code = $progress->code;
             fwrite($file, $code);
             // can I send the file to Laravel storage?
-            $filePath = Storage::disk('local')->putFile('submissions/'. $user->fsc_id . "/code/" . $id, file_get_contents("submission.py"));
+            $filePath = Storage::disk('local')->putFile('submissions/'. $user->fsc_id . "/code/" . $id, new File("submission.py"));
             fclose($file);
             return response()->json(['message' => 'Python code stored.', 'path' => $filePath], 200);
         } else {
