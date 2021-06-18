11423aedf4180153f7fa175e7b27d70ff1356dcc
maxb00 <maxabr00@yahoo.com>
2021-06-18 13:13:39 -0400

    no cluster

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,7 +23,7 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: 'mocside.com',
     wsPort: 6001,
     forceTLS: false,
