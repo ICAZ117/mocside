578f510239eb8ed4a5cf8d01f19b7bfc18b682d1
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-21 13:02:34 -0400

    Testing

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -1,46 +1,103 @@
 <template>
-  <div>
-    <nav class="navbar bg-light navbar-light navbar-expand-xl">
-      <router-link to="/" class="navbar-brand">
-        <img src="../../img/logo/brandlight.png" alt="Mocs-IDE" title="Logo" />
-      </router-link>
+  <div v-if="!isDark">
+    <div>
+      <nav class="navbar bg-light navbar-light navbar-expand-xl">
+        <router-link to="/" class="navbar-brand">
+          <img src="../../img/logo/brandlight.png" alt="Mocs-IDE" title="Logo" />
+        </router-link>
 
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarResponsive"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse nav-items" id="navbarResponsive">
-        <div class="navbar-nav">
-          <div class="nav-item">
-            <router-link @click="update" to="/" class="nav-link">Home</router-link>
-            <router-link v-if="isLoggedIn" @click="update" to="/courses" class="nav-link"
-              >Courses</router-link
-            >
-            <router-link v-if="!isLoggedIn" @click="update" to="/login" class="nav-link"
-              >Login</router-link
-            >
-            <router-link
-              v-if="!isLoggedIn"
-              @click="update"
-              to="/register"
-              class="nav-link"
-              >Sign Up</router-link
-            >
-            <router-link v-if="isLoggedIn" @click="update" to="/about" class="move-up"
-              ><img class="pfp" src="../../img/DefaultPFP.png" alt="Profile"
-            /></router-link>
-            <a @click="logout" v-if="isLoggedIn" class="nav-link">Logout</a>
+        <div class="collapse navbar-collapse nav-items" id="navbarResponsive">
+          <div class="navbar-nav">
+            <div class="nav-item">
+              <router-link @click="update" to="/" class="nav-link">Home</router-link>
+              <router-link
+                v-if="isLoggedIn"
+                @click="update"
+                to="/courses"
+                class="nav-link"
+                >Courses</router-link
+              >
+              <router-link v-if="!isLoggedIn" @click="update" to="/login" class="nav-link"
+                >Login</router-link
+              >
+              <router-link
+                v-if="!isLoggedIn"
+                @click="update"
+                to="/register"
+                class="nav-link"
+                >Sign Up</router-link
+              >
+              <router-link v-if="isLoggedIn" @click="update" to="/about" class="move-up"
+                ><img class="pfp" src="../../img/DefaultPFP.png" alt="Profile"
+              /></router-link>
+              <a @click="logout" v-if="isLoggedIn" class="nav-link">Logout</a>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
 
-    <div class="navbar-spacer"></div>
+      <div class="navbar-spacer"></div>
+    </div>
+
+
+
+    <!-- DARK NAVBAR -->
+    <div v-if="isDark">
+      <nav class="dark-navbar bg-light navbar-light navbar-expand-xl">
+        <router-link to="/" class="navbar-brand">
+          <img src="../../img/logo/brandlight.png" alt="Mocs-IDE" title="Logo" />
+        </router-link>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse nav-items" id="navbarResponsive">
+          <div class="navbar-nav">
+            <div class="nav-item">
+              <router-link @click="update" to="/" class="dark-nav-link">Home</router-link>
+              <router-link
+                v-if="isLoggedIn"
+                @click="update"
+                to="/courses"
+                class="dark-nav-link"
+                >Courses</router-link
+              >
+              <router-link v-if="!isLoggedIn" @click="update" to="/login" class="dark-nav-link"
+                >Login</router-link
+              >
+              <router-link
+                v-if="!isLoggedIn"
+                @click="update"
+                to="/register"
+                class="dark-nav-link"
+                >Sign Up</router-link
+              >
+              <router-link v-if="isLoggedIn" @click="update" to="/about" class="dark-move-up"
+                ><img class="pfp" src="../../img/DefaultPFP.png" alt="Profile"
+              /></router-link>
+              <a @click="logout" v-if="isLoggedIn" class="nav-link">Logout</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div class="navbar-spacer"></div>
+    </div>
   </div>
 </template>
 
@@ -49,7 +106,7 @@ export default {
   props: ["authUser"],
   data() {
     return {
-      dark: "",
+      isDark: false,
     };
   },
   methods: {
@@ -71,11 +128,13 @@ export default {
     },
   },
   watch: {
-    $route(to, from) {
-      console.log("\nTo:");
-      console.log(to);
-      console.log("\nFrom:");
-      console.log(from);
+    $route(to) {
+      if (to.fullPath != "/") {
+        isDark = true;
+      }
+      else {
+        isDark = false;
+      }
     },
   },
   mounted() {
