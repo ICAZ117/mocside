6592f7437546364eb923fe3c9a7c2e44322f68df
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-25 13:26:35 -0400

    More Testing

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,36 +1,39 @@
 <template>
   <div>
-    <Carousel class="carousel-inner" :itemsToShow="1" :snapAlign="center" :wrapAround="true">
-        <!-- Carousel Item 1 -->
-        <Slide style="background-image: url('../../img/carousel/1.jpg')">
-          <div class="carousel__item">
-            <img src="../../img/carousel/1.jpg" alt="Image carousel" class="w-100" />
+    <Carousel
+      class="carousel-inner"
+      :itemsToShow="1"
+      :snapAlign="center"
+      :wrapAround="true"
+    >
+      <!-- Carousel Item 1 -->
+      <Slide style="background-image: url('../../img/carousel/1.jpg')">
+        <div class="carousel__item">
+          <div class="carousel-caption">
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-8 bg-custom d-none d-md-block py-3 px-0">
+                  <h1>Mocs-IDE</h1>
 
-            <div class="carousel-caption">
-              <div class="container">
-                <div class="row justify-content-center">
-                  <div class="col-8 bg-custom d-none d-md-block py-3 px-0">
-                    <h1>Mocs-IDE</h1>
+                  <div class="border-top border-danger w-50 mx-auto my-3"></div>
 
-                    <div class="border-top border-danger w-50 mx-auto my-3"></div>
+                  <h4>A modern, free to use online IDE for virtual classrooms</h4>
 
-                    <h4>A modern, free to use online IDE for virtual classrooms</h4>
+                  <router-link to="/login" class="btn btn-danger btn-lg mr-2"
+                    >Login</router-link
+                  >
 
-                    <router-link to="/login" class="btn btn-danger btn-lg mr-2"
-                      >Login</router-link
-                    >
-
-                    <router-link to="/register" class="btn btn-danger btn-lg ml-2"
-                      >Sign Up</router-link
-                    >
-                  </div>
+                  <router-link to="/register" class="btn btn-danger btn-lg ml-2"
+                    >Sign Up</router-link
+                  >
                 </div>
               </div>
             </div>
           </div>
-        </Slide>
+        </div>
+      </Slide>
 
-        <!-- Carousel Item 2 -->
+      <!-- Carousel Item 2
         <Slide style="background-image: url('../../img/carousel/2.jpg')">
           <div class="carousel__item">
 
@@ -52,26 +55,25 @@
           </div>
         </Slide>
 
-        <!-- Carousel Item 3 -->
-        <Slide style="background-image: url('../../img/carousel/3.jpg')">
-          <div class="carousel__item">
+      Carousel Item 3
+      <Slide style="background-image: url('../../img/carousel/3.jpg')">
+        <div class="carousel__item">
+          <div class="carousel-caption">
+            <div class="container">
+              <div class="row justify-content-start text-left">
+                <div class="col-7 bg-custom d-none d-lg-block py-3 px-0 pl-3">
+                  <p class="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, nulla?
+                  </p>
 
-            <div class="carousel-caption">
-              <div class="container">
-                <div class="row justify-content-start text-left">
-                  <div class="col-7 bg-custom d-none d-lg-block py-3 px-0 pl-3">
-                    <p class="lead">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
-                      nulla?
-                    </p>
-
-                    <a href="" class="btn btn-primary btn-lg">Button</a>
-                  </div>
+                  <a href="" class="btn btn-primary btn-lg">Button</a>
                 </div>
               </div>
             </div>
           </div>
-        </Slide>
+        </div>
+      </Slide>
+      -->
 
       <template #addons>
         <navigation />
@@ -113,4 +115,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  color: var(--carousel-color-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+  margin: 0 20px;
+  background-color: transparent!important;
+}
+</style>
