f836e4599dfd6d725a2716deefa8739e794e6e84
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-29 12:19:33 -0400

    more testing

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,3 +1,47 @@
+<!--<template>
+  <textarea contenteditable="true"
+    v-model="contents"
+    @keyup.enter="enter"
+    spellcheck="false"></textarea>
+</template>
+
+<script>
+import * as API from "../services/API";
+
+export default {
+  data() {
+    return {
+      containerID: 0,
+      contents: "",
+      oldContents: "",
+    }
+  },
+  async mounted() {
+    var payload = {
+      lang: 'python',
+    }
+    const res = await API.apiClient.post(`/containers/test/23`, payload);
+    this.containerID = res.data.message;
+    const attach = await API.apiClient.get(`/containers/test/${this.containerID}`)
+  },
+  methods: {
+    async enter() {
+      this.newInput = this.contents.substring(this.oldContents.length);
+      var payload = {
+        input: this.newInput,
+      };
+
+      const res = await API.apiClient.post(
+        `/containers/test-in/${this.containerID}`,
+        payload
+      );
+    }
+  }
+};
+</script>
+
+<style></style> -->
+
 <template>
   <VueFaqAccordion :items="myItems" />
 </template>
@@ -32,3 +76,9 @@ export default {
   },
 };
 </script>
+
+<style>
+* {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+</style>
