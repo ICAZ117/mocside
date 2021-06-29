697b2deeb40c71ac42b43f6f93335175803f688f
maxb00 <maxabr00@yahoo.com>
2021-06-29 12:55:42 -0400

    stop

--- resources/js/Components/Console.vue
+++ resources/js/Components/Console.vue
@@ -190,7 +190,7 @@ export default {
             // self.checkLogs();
           } 
         } else if (self.isWaiting) {
-          // self.checkLogs();
+          self.checkLogs();
         }
       }, 1000);
     }
