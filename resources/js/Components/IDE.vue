7df921bfd36da9679e989ba3f636151fda5ee54f
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-29 12:48:54 -0400

    Added styling to modal

--- resources/js/Components/IDE.vue
+++ resources/js/Components/IDE.vue
@@ -35,13 +35,7 @@
           v-model="showModal"
           classes="modal-container"
           content-class="modal-content"
-          :max-width="600"
-          :min-width="600"
-          transition="{'enter-active-class': 'transition duration-200 ease-in-out transform','enter-from-class': 'translate-y-full','enter-to-class': 'translate-y-0','leave-active-class': 'transition duration-200 ease-in-out transform','leave-to-class': 'translate-y-full','leave-from-class': 'translate-y-0'}"
-          overlay-transition="{'enter-active-class': 'transition duration-200 ease-in-out transform','enter-from-class': 'translate-y-full','enter-to-class': 'translate-y-0','leave-active-class': 'transition duration-200 ease-in-out transform','leave-to-class': 'translate-y-full','leave-from-class': 'translate-y-0'}"
           :esc-to-close="true"
-          :min-height="800"
-          :max-height="800"
         >
           <Accordion />
           <button class="modal-close" @click="showModal = false">x</button>
@@ -336,4 +330,49 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+::v-deep(.modal-container) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+::v-deep(.modal-content) {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  max-height: 90%;
+  margin: 0 1rem;
+  padding: 1rem;
+  border: 1px solid #e2e8f0;
+  border-radius: 0.25rem;
+  background: #fff;
+}
+.modal__title {
+  margin: 0 2rem 0 0;
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+.modal__content {
+  flex-grow: 1;
+  overflow-y: auto;
+}
+.modal__action {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  padding: 1rem 0 0;
+}
+.modal__close {
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+}
+</style>
+
+<style scoped>
+.dark-mode div ::v-deep(.modal-content) {
+  border-color: #2d3748;
+  background-color: #1a202c;
+}
+</style>
