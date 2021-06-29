08f26e8a3588d7855f2cfee50a28bb67aee64a4a
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-29 12:30:24 -0400

    Testing custom accordion component

---
+++ resources/js/Components/Accordion.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div v-for="accordion in accordions" :key="accordion.title">
+      <BaseAccordian>
+        <template v-slot:title>{{ accordion.title }}</template>
+
+        <template v-slot:content>
+          <div>{{ accordion.text }}</div>
+        </template>
+      </BaseAccordian>
+    </div>
+  </div>
+</template>
+<script>
+import BaseAccordian from "./BaseAccordion.vue";
+
+export default {
+  components: {
+    BaseAccordian,
+  },
+  data() {
+    return {
+      accordions: [
+        {
+          title: "Vue",
+          text: "All about Vue",
+        },
+        {
+          title: "Nuxt",
+          text: "All about Nuxt",
+        },
+        {
+          title: "webpack",
+          text: "All about webpack",
+        },
+      ],
+    };
+  },
+};
+</script>
--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -43,42 +43,29 @@ export default {
 <style></style> -->
 
 <template>
-  <VueFaqAccordion :items="myItems" />
+  <div>
+    <h1>Accordion</h1>
+    <Accordion/>
+  </div>
 </template>
 
 <script>
-import VueFaqAccordion from "vue-faq-accordion";
+import Accordion from "../Components/Accordion.vue";
 
 export default {
   components: {
-    VueFaqAccordion,
-  },
-  data() {
-    return {
-      myItems: [
-        {
-          title: "How many time zones are there in all?",
-          value: "Given a 24-hour day and 360 degrees of longitude around the Earth",
-          category: "Tab-1",
-        },
-        {
-          title: "How long is a day and year on Venus?",
-          value: "Venus takes 224.7 Earth days to complete one orbit around the Sun.",
-          category: "Tab-2",
-        },
-        {
-          title: "What animal smells like popcorn?",
-          value: "Binturongs smell like popcorn.",
-          category: "Tab-2",
-        },
-      ],
-    };
-  },
+    Accordion
+  }
 };
 </script>
 
-<style>
+<style scoped>
 * {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
