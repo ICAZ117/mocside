61ca14664bf6b94f6fdfbe27a9708d6e50066c54
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-30 12:20:21 -0400

    Testing

--- resources/js/Components/Accordion.vue
+++ resources/js/Components/Accordion.vue
@@ -3,24 +3,12 @@
     <div v-for="accordion in accordions" :key="accordion.title">
       <div>
         <!-- IF code passed TC, OR if code failed TC due to an error-->
-        <BaseAccordion
-          v-if="accordion.isSuccessfull || (!accordion.isSuccessful && accordion.hasError)"
-          :isSuccessful="accordion.isSuccessful"
-        >
-        eeeeeeeeee
+        <BaseAccordion :isSuccessful="accordion.isSuccessful">
+          eeeeeeeeee
           <template v-slot:title>{{ accordion.title }}</template>
 
           <template v-slot:content>
-            <div>{{ accordion.text }}</div>
-          </template>
-        </BaseAccordion>
-
-        <!-- ELSE, code failed test case due to logical issue, must display comparison -->
-        <BaseAccordion v-if="!accordion.isSuccessful && !accordion.hasError" :isSuccessful="accordion.isSuccessful">
-          <template v-slot:title>{{ accordion.title }}</template>
-
-          <template v-slot:content>
-            <div class="wrapper">
+            <div v-if="!accordion.isSuccessful && !accordion.hasError" class="wrapper">
               <div>{{ accordion.text }}</div>
               <br />
 
@@ -40,6 +28,8 @@
               <div class="info" v-html="accordion.differences"></div>
               <br />
             </div>
+
+            <div v-else>{{ accordion.text }}</div>
           </template>
         </BaseAccordion>
       </div>
