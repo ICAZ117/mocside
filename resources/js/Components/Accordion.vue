da8e3cf8213e91b4ed4592eedbc73b80fd7e0897
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-06-30 15:39:57 -0400

    Last commit for today, finished submissions

--- resources/js/Components/Accordion.vue
+++ resources/js/Components/Accordion.vue
@@ -6,9 +6,10 @@
           <template v-slot:title>{{ accordion.title }}</template>
 
           <template v-slot:content>
+            <div>{{ accordion.text }}</div>
+
             <!-- IF the code FAILED the test case, and does NOT have an error, print extensive output -->
             <div v-if="!accordion.isSuccessful && !accordion.hasError" class="details">
-              <div>{{ accordion.text }}</div>
               <br />
 
               <p>Input:</p>
@@ -27,9 +28,6 @@
               <div class="info" v-html="accordion.differences"></div>
               <br />
             </div>
-
-            <!-- ELSE, the code passed the test case, or there is an error. Simply print the text. -->
-            <div v-else>{{ accordion.text }}</div>
           </template>
         </BaseAccordion>
       </div>
