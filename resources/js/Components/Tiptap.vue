9cf493544c0db922a27f756e53a5330222e48a4a
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-07-01 13:16:40 -0400

    Testing a bug fix in Tiptap

--- resources/js/Components/Tiptap.vue
+++ resources/js/Components/Tiptap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="height: 100%!important;">
+  <div style="height: fit-content!important;">
     <bubble-menu
       class="bubble-menu"
       :tippy-options="{ duration: 100 }"
