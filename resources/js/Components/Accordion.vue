254e1f05c8f16ea5a87c9e5680ed8c905931b72d
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-07-01 11:51:04 -0400

    testing

--- resources/js/Components/Accordion.vue
+++ resources/js/Components/Accordion.vue
@@ -3,7 +3,10 @@
     <div v-for="accordion in accordions" :key="accordion.title">
       <div>
         <BaseAccordion :isSuccessful="accordion.isSuccessful">
-          <template v-slot:title>{{ accordion.title }}</template>
+          <template v-slot:title>
+            {{ accordion.title }}
+            
+          </template>
 
           <template v-slot:content>
             <div>{{ accordion.text }}</div>
