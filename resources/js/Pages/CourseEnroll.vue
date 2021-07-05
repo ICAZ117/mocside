4ae7b3a3fddef57ef7439f48c02f1cf69b16ac0c
Stephen Chase Robinson <srobinson6@mocs.flsouthern.edu>
2021-07-05 13:54:25 -0400

    renaming methods

--- resources/js/Pages/CourseEnroll.vue
+++ resources/js/Pages/CourseEnroll.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>You have been invited to join {{ course.name }}</h1>
-  <button @click="join()">Join</button>
-  <button @click="cancel()" >Cancel</button>
+  <button @click="joinCourse()">Join</button>
+  <button @click="cancelCourse()" >Cancel</button>
   <p>yah</p>
 </template>
 
@@ -13,14 +13,14 @@ export default {
             course: {},
         }
     },
-    join() {
+    joinCourse() {
         console.log("join");
     },
-    cancel() {
+    cancelCourse() {
         console.log("cancel");
     },
     async mounted() {
-        const res = await API.apiClient.get(`this is the backend route for each course invitation/${courseKey}`);
+        // const res = await API.apiClient.get(`this is the backend route for each course invitation/${courseKey}`);
     }
 }
 </script>
