996e8e295c9a03e2f44d8f340bbf9fbb11e2b792
maxb00 <maxabr00@yahoo.com>
2021-07-06 18:00:08 -0400

    revert IDE change

--- resources/js/Components/IDE.vue
+++ resources/js/Components/IDE.vue
@@ -361,8 +361,7 @@ export default {
         }
         // ELSE, the code ran successfully. Now check if it passed the test case or not.
         else {
-          // tc.compare = JSON.parse(tc.compare);
-          console.log(JSON.parse(tc.compare));
+          tc.compare = JSON.parse(tc.compare);
 
           // IF code passed test case
           if (tc.compare[0] == "100.0") {
