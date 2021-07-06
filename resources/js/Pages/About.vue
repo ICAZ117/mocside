e48209034a2fc2b9b9fe0aab4cfc0b2e342918fe
Stephen Chase Robinson <srobinson6@mocs.flsouthern.edu>
2021-07-06 19:46:36 -0400

    testing

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,87 +1,99 @@
 <template>
-  <div style="opacity: 1; transform: scale(1) translateY(0px) translateZ(0px)">
-    <div>
-      <section class="authBox-hW6HRx theme-dark">
-        <div class="centeringWrapper-2Rs1dR">
-          <div class="container-23z5yI">
-            <svg
-              width="64"
-              height="64"
-              class="guildIcon-1PRDp6"
-              viewBox="0 0 64 64"
-            >
-              <foreignObject
-                x="0"
-                y="0"
+  <div class="wrapper-6URcxg">
+    <div
+      style="opacity: 1; transform: scale(1) translateY(0px) translateZ(0px)"
+    >
+      <div>
+        <section class="authBox-hW6HRx theme-dark">
+          <div class="centeringWrapper-2Rs1dR">
+            <div class="container-23z5yI">
+              <svg
                 width="64"
                 height="64"
-                overflow="visible"
-                mask="url(#svg-mask-squircle)"
-                ><div
-                  class="
-                    icon-3o6xvg
-                    iconSizeLarger-2MnpN2
-                    iconActiveLarger-1L4Joi
-                  "
-                  style="
-                    background-image: url('https://cdn.discordapp.com/icons/844527946702127144/3d216bbe445d09dc25408d711ec4db89.png?size=256');
-                  "
-                  tabindex="0"
-                  role="button"
-                ></div
-              ></foreignObject>
-            </svg>
-            <div class="colorHeaderSecondary-3Sp3Ft size16-1P40sf">
-              Discord.Me invited you to join
+                class="guildIcon-1PRDp6"
+                viewBox="0 0 64 64"
+              >
+                <foreignObject
+                  x="0"
+                  y="0"
+                  width="64"
+                  height="64"
+                  overflow="visible"
+                  mask="url(#svg-mask-squircle)"
+                  ><div
+                    class="
+                      icon-3o6xvg
+                      iconSizeLarger-2MnpN2
+                      iconActiveLarger-1L4Joi
+                    "
+                    style="
+                      background-image: url('https://cdn.discordapp.com/icons/844527946702127144/3d216bbe445d09dc25408d711ec4db89.png?size=256');
+                    "
+                    tabindex="0"
+                    role="button"
+                  ></div
+                ></foreignObject>
+              </svg>
+              <div class="colorHeaderSecondary-3Sp3Ft size16-1P40sf">
+                Discord.Me invited you to join
+              </div>
+              <h3 class="title-jXR8lp title-2_aCTw base-1x0h_U size24-RIRrxO">
+                BTC-Bitcoin
+              </h3>
+              <div
+                style="flex: 1 1 auto"
+                class="
+                  flex-1xMQg5 flex-1O1GKY
+                  horizontal-1ae9ci horizontal-2EEEnY
+                  flex-1O1GKY
+                  directionRow-3v3tfG
+                  justifyCenter-3D2jYp
+                  alignStretch-DpGPf3
+                  noWrap-3jynv6
+                  activityCount-2GgGMq
+                "
+              >
+                <div class="pill-1dHPfk pillOnline-35Y-7N">
+                  <i class="pillIconOnline-2JGlqR pillIcon-BaqtHs"></i
+                  ><span
+                    class="
+                      colorStandard-2KCXvj
+                      size16-1P40sf
+                      pillMessage-1btqlx
+                    "
+                    >15 Online</span
+                  >
+                </div>
+                <div class="pill-1dHPfk">
+                  <i class="pillIconTotal-1BqTcA pillIcon-BaqtHs"></i
+                  ><span
+                    class="
+                      colorStandard-2KCXvj
+                      size16-1P40sf
+                      pillMessage-1btqlx
+                    "
+                    >106 Members</span
+                  >
+                </div>
+              </div>
             </div>
-            <h3 class="title-jXR8lp title-2_aCTw base-1x0h_U size24-RIRrxO">
-              BTC-Bitcoin
-            </h3>
-            <div
-              style="flex: 1 1 auto"
+            <button
+              type="button"
               class="
-                flex-1xMQg5 flex-1O1GKY
-                horizontal-1ae9ci horizontal-2EEEnY
-                flex-1O1GKY
-                directionRow-3v3tfG
-                justifyCenter-3D2jYp
-                alignStretch-DpGPf3
-                noWrap-3jynv6
-                activityCount-2GgGMq
+                marginTop40-i-78cZ
+                button-3k0cO7 button-38aScr
+                lookFilled-1Gx00P
+                colorBrand-3pXr91
+                sizeLarge-1vSeWK
+                fullWidth-1orjjo
+                grow-q77ONN
               "
             >
-              <div class="pill-1dHPfk pillOnline-35Y-7N">
-                <i class="pillIconOnline-2JGlqR pillIcon-BaqtHs"></i
-                ><span
-                  class="colorStandard-2KCXvj size16-1P40sf pillMessage-1btqlx"
-                  >15 Online</span
-                >
-              </div>
-              <div class="pill-1dHPfk">
-                <i class="pillIconTotal-1BqTcA pillIcon-BaqtHs"></i
-                ><span
-                  class="colorStandard-2KCXvj size16-1P40sf pillMessage-1btqlx"
-                  >106 Members</span
-                >
-              </div>
-            </div>
+              <div class="contents-18-Yxp">Accept Invite</div>
+            </button>
           </div>
-          <button
-            type="button"
-            class="
-              marginTop40-i-78cZ
-              button-3k0cO7 button-38aScr
-              lookFilled-1Gx00P
-              colorBrand-3pXr91
-              sizeLarge-1vSeWK
-              fullWidth-1orjjo
-              grow-q77ONN
-            "
-          >
-            <div class="contents-18-Yxp">Accept Invite</div>
-          </button>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   </div>
 </template>
