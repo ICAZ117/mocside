a88b9056e16e0ebfebabc72ec3ad0daed2edc647
Stephen Chase Robinson <srobinson6@mocs.flsouthern.edu>
2021-07-09 15:02:42 -0400

    updated general section of About Us

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,9 +1,21 @@
 <template>
+<h1>About Us</h1>
+<div name="TheTeam">
+    <p>The Orang Carrots, the team behind this site, mocside.com, is a group of three Florida Southern Students, one sophmore and two juniors. The original spark behind this project was another site, repl.it removing its classroom functionality in the spring of 2021. We decided to pool our resources and time and create our own better version of the site, for use in the florida Southern CS department. Within the world of Cs one of the best ways to learn is repition, and it can be hard to grade yuor projects on your own. It also gets out of hand very quickly for a professor or a teachers assistant to grade student code. That is where this site shines, it gives professors that ability to create a course with problems, and individual test cases for each problem. Students can then log in to the course and write code on the fully functional code editor, and then submit and run their code. All without the need to ensure that the student has the latest java or python installed on their machine. The site will then autgrade their results and return those results back to the user and save them. then once the professor is ready to transfer grades they can easily see all the grades for an entire course, lab or individual assignment or student. This allows professors to focus more on teaching and less on grading all the small assignments of each individual student.</p>
+</div>
+<section name="Chase"></section>
+<section name="Max"></section>
+<section name="Ibraheem"></section>
+<section name="Dr. Cazalas"></section>
+
+<section name="achievements"></section>
+
+
+<section name="Contact US"></section>
   
 </template>
 
 <script>
-console.log(new Date(Date.UTC(2021, 0, 31, 25, 0, 0, 0)));
 </script>
 
 <style>
