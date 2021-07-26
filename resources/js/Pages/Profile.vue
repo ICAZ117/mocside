3dbfebfcd47450b9ef7a9062b476ec1f1113d115
Vbuddies <cheesychase@myexcel.com>
2021-07-26 11:40:18 -0400

    adding basic tabs to profile page

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -1,22 +1,55 @@
 <template>
-
+  <div class="courses header">
+    <div class="heading">
+      <tabs v-model="selectedTab">
+        <tab
+          class="tab"
+          v-for="(tab, i) in tabs"
+          :key="`t${i}`"
+          :val="tab"
+          :label="tab"
+          :indicator="true"
+        />
+      </tabs>
+    </div>
+  </div>
+  <tab-panels v-model="selectedTab" :animate="true">
+      <tab-panel :val="'Profile'">
+        Profile
+      </tab-panel>
+      <tab-panel :val="'Grades'">
+        Grades
+      </tab-panel>
+      <tab-panel :val="'Security'">
+        Security
+      </tab-panel>
+  </tab-panels>
 </template>
 
 <script>
-export default {};
+const tabs = ["Profile", "Grades", "Security"];
+export default {
+  setup() {
+    const route = useRoute();
+
+    const currentDirectory = computed(() => route.path);
+
+    const state = reactive({
+      selectedTab: tabs[0],
+    });
+
+    return {
+      currentDirectory,
+      tabs,
+      ...toRefs(state),
+    };
+  },
+};
 // * Add profile Page....sorta like a dashboard
 // * Overall Course Grades
 // * Profile Settings.....username, email, password, profile picture, delete account?...if professor upgrade button
 // * IDE Settings......Theme, language, Console Theme
 
-//i'm imagining a tab with the tab panels on the left side of the screen in a column
-//list of tabs in descending order
-//overview, grades, security, profile(maybe this should be on overview), IDE/Console
-//in overview there will be cards to access the different tabs, without using the links in the left side
-//the overview card will have a profile picture, screen name, full name, perhaps a delete button at the bottom
-//grades will show the overall grades for all past and present courses, and pehaps a small column of recents on the right to show recently graded projects
-//security will have change username, change password, change email
-//IDE will allow the user to change the theme of ace, change the default language(is there any need for this?), and change the console theme, once we implement that
 </script>
 
 <style>
