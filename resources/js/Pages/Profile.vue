62bdea311a5dca32e7c56fb5932deeeb47be5655
Vbuddies <cheesychase@myexcel.com>
2021-07-29 12:07:37 -0400

    adding file upload

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -16,7 +16,13 @@
   <tab-panels v-model="selectedTab" :animate="true">
       <tab-panel :val="'Profile'">
         <div class="profile-section">
-          <div class="picture">
+          <div class="picture"><label for="file" class="sr-only"> Upload Course Image </label>
+            <input
+              type="file"
+              :accept="['image/*']"
+              @change="fileChange"
+              id="file"
+            />
             <button class="btn btn-danger btn-block">Change Avatar</button>
           </div>
           <div class="editable">
@@ -163,10 +169,15 @@
 import * as API from "../services/API";
 import store from "../Store/index";
 import { useRoute } from "vue-router";
+import FileService from "../services/FileService";
+import FileUpload from "../Components/FileUpload";
 import { defineComponent, reactive, toRefs, computed } from "vue";
 
 const tabs = ["Profile", "Grades", "Security"];
 export default {
+  components: {
+    FileUpload,
+  },
   data() {
     return {
       authUser: null,
@@ -226,6 +237,35 @@ export default {
       this.user.username = this.authUser.username;
       this.user.fsc_id = this.authUser.fsc_user.fsc_id;
       this.user.pfp = "";
+    },
+    clearMessage() {
+      this.error = null;
+      this.message = null;
+    },
+    fileChange(event) {
+      this.clearMessage();
+      this.file = event.target.files[0];
+    },
+    async uploadImage() {
+      if (this.file != null) {
+        const payload = {};
+        const formData = new FormData();
+        formData.append("file", this.file);
+        payload.file = formData;
+        payload.endpoint = "/images/store";
+        this.clearMessage();
+        try {
+          const response = await FileService.uploadFile(payload);
+          this.message = "File uploaded.";
+          console.log(response.data.asset_link);
+          this.user.pfp = response.data.asset_link;
+        } catch (error) {
+          this.error = getError(error);
+        }
+      }
+    },
+    saveProfile() {
+      await this.uploadImage();
     },
     getGrades() {
       for(let i = 0; i < this.enrolledCourses.length; i++) {
