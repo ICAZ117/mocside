c0f812d2d0ad3de3381b1ff2d3cc4edfa21068b7
Vbuddies <cheesychase@myexcel.com>
2021-08-02 11:22:58 -0400

    updating where we get pronouns from

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -262,7 +262,7 @@ export default {
       this.user.fsc_id = this.authUser.fsc_user.fsc_id;
 
       //make sure this is how they are being received from database
-      this.user.pronouns = this.authUser.pronouns;
+      this.user.pronouns = this.authUser.fsc_user.pronouns;
       this.user.settings = this.authUser.settings;
 
       this.user.pfp = this.authUser.pfp_path;
