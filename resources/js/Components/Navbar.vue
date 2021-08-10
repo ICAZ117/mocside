2d5aa215f1a19a84072f461cebb3ca29dc264902
Vbuddies <cheesychase@myexcel.com>
2021-08-10 09:48:52 -0400

    reverting last change

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -132,9 +132,7 @@ export default {
     async updatePFP() {
       var au = await this.$store.dispatch("auth/getAuthUser");
       if(this.authUser != null) {
-        console.log(au.pfp_path);
         this.pfp = au.pfp_path;
-        console.log(this.pfp);
         if(this.pfp == undefined || this.pfp == null) {
           this.pfp = "images/DefaultPFP.png?dca25dcd82b7a37cf8c8334dbf19eb69=";
         }
@@ -143,7 +141,7 @@ export default {
     },
   },
   mounted() {
-    // this.updatePFP();
+    this.updatePFP();
   },
   computed: {
     isLoggedIn: function () {
