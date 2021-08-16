b3d0d0388ffaa33ba2668dbcb3caa5e6d2a1dfba
maxb00 <barlowm15@gmail.com>
2021-08-16 12:50:06 -0400

    missed import

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use App\Models\Professor;
+use App\Models\Student;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
