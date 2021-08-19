1f08cabfcae6681bdce5cbaa855723e9747c42e9
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-08-19 12:45:21 -0400

    Testing watch

--- resources/js/Components/IDE.vue
+++ resources/js/Components/IDE.vue
@@ -319,7 +319,7 @@ export default defineComponent({
       console.log(this.width);
       console.log("\nw2:");
       console.log(this.w2);
-      this.w2 = this.width;
+      // this.w2 = this.width;
     }
   },
   methods: {
