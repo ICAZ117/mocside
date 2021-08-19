376520bc38c0c8801dea39d9095cccda74b925e0
Vbuddies <cheesychase@myexcel.com>
2021-08-19 11:22:07 -0400

    testing

--- resources/js/Pages/EditCourse.vue
+++ resources/js/Pages/EditCourse.vue
@@ -350,7 +350,7 @@ export default {
 </script>
 
 <style>
-.course-dashboard {
+/* .course-dashboard {
   width: 100%;
   height: 90%;
 }
@@ -371,5 +371,5 @@ export default {
 }
 .labs {
   width: 45.5%;
-}
+} */
 </style>
