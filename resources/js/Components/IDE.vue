be07d3bf677357c95e5a548f13aca143b04bb346
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-08-19 12:49:35 -0400

    Testing manual overide of VDR width

--- resources/js/Components/IDE.vue
+++ resources/js/Components/IDE.vue
@@ -319,6 +319,7 @@ export default defineComponent({
       console.log(this.width);
       console.log("\nw2:");
       console.log(this.w2);
+      document.getElementById("resizeableIDE").style.width = this.width + "px!important";
       // this.w2 = this.width;
     }
   },
