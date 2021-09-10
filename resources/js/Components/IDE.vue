2e306863e2ac3dba9525ce98bb16465d7b836d2e
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-09-10 11:39:11 -0400

    Changed the h2 calculation back to -60

--- resources/js/Components/IDE.vue
+++ resources/js/Components/IDE.vue
@@ -364,7 +364,7 @@ export default defineComponent({
     h1: function () {
       setTimeout(() => {
         this.y2 = this.offsetTop + this.h1;
-        this.h2 = this.h2 - 1;
+        this.h2 = this.h2 - 60;
       }, 10);
     },
   },
