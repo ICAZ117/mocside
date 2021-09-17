7540d000efa638ce355d3b0705931b2075bac7ab
Chase Robinson <cheesychase@myexcel.com>
2021-09-17 10:45:57 -0400

    added class to buttons

--- resources/js/Pages/Courses.vue
+++ resources/js/Pages/Courses.vue
@@ -12,8 +12,10 @@
         <button class="modal-close" @click="closeDeleting()">x</button>
         <div class="delete Course">
           <p>Are you sure you would like to delete {{ deletingCourse.course.name }}</p>
-          <button class="btn btn-md btn-danger" @click="closeDeleting()">Cancel</button>
-          <button class="btn btn-md btn-danger" @click="deleteCourse()">Delete</button>
+          <div class="delete-buttons">
+            <button class="btn btn-md btn-danger" @click="closeDeleting()">Cancel</button>
+            <button class="btn btn-md btn-danger" @click="deleteCourse()">Delete</button>
+          </div>
         </div>
       </vue-final-modal>
       <div class="courses header">
