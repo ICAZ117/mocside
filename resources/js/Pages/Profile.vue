4ae42984369f03c507c00729f191090c4ce05e99
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-10-01 11:15:31 -0400

    Moved stuff into rows and columns

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -15,92 +15,98 @@
   </div>
   <tab-panels v-model="selectedTab" :animate="true">
     <tab-panel :val="'Profile'">
-      <div class="profile-picture">
-        <img class="large-pfp" src="this.user.pfp" alt="Profile" id="pfp" />
-        <button @click="editAvatar()" class="btn btn-danger btn-block">Edit</button>
-      </div>
-      <div class="profile-section">
-        <div class="editable">
-          <label for="Name">Name</label>
-          <input type="text" v-model="user.name" id="Name" />
-          <label for="ScreenName">ScreenName</label>
-          <input type="text" v-model="user.screen_name" id="ScreenName" />
-          <label for="FSCID">FSC ID</label>
-          <input type="number" v-model="user.fsc_id" id="FSCID" disabled />
-          <label for="pronouns">Preferred Pronouns</label>
-          <input type="text" name="pronouns" id="pronouns" v-model="user.pronouns" />
+      <div class="row">
+        <div class="col-4">
+          <div class="profile-picture">
+            <img class="large-pfp" src="this.user.pfp" alt="Profile" id="pfp" />
+            <button @click="editAvatar()" class="btn btn-danger btn-block">Edit</button>
+          </div>
         </div>
-      </div>
-      <div class="Editor-Settings">
-        <!-- theme, language, console theme -->
-        <label for="Theme">Select A Default Theme</label>
-        <select name="Theme" id="Theme" v-model="user.settings.ideOptions.theme">
-          <optgroup label="Dark">
-            <option value="ambiance">Ambiance</option>
-            <option value="chaos">Chaos</option>
-            <option value="clouds_midnight">Clouds Midnight</option>
-            <option value="dracula">Dracula</option>
-            <option value="cobalt">Cobalt</option>
-            <option value="gruvbox">Gruvbox</option>
-            <option value="gob" selected>Green on Black</option>
-            <option value="idle_fingers">idle Fingers</option>
-            <option value="kr_theme">krTheme</option>
-            <option value="merbivore">Merbivore</option>
-            <option value="merbivore_soft">Merbivore Soft</option>
-            <option value="mono_industrial">Mono Industrial</option>
-            <option value="monokai">Monokai</option>
-            <option value="nord_dark">Nord Dark</option>
-            <option value="pastel_on_dark">Pastel on dark</option>
-            <option value="solarized_dark">Solarized Dark</option>
-            <option value="terminal">Terminal</option>
-            <option value="tomorrow_night">Tomorrow Night</option>
-            <option value="tomorrow_night_blue">Tomorrow Night Blue</option>
-            <option value="tomorrow_night_bright">Tomorrow Night Bright</option>
-            <option value="tomorrow_night_eighties">Tomorrow Night 80s</option>
-            <option value="twilight">Twilight</option>
-            <option value="vibrant_ink">Vibrant Ink</option>
-          </optgroup>
-          <optgroup label="Light">
-            <option value="chrome">Chrome</option>
-            <option value="clouds">Clouds</option>
-            <option value="crimson_editor">Crimson Editor</option>
-            <option value="dawn">Dawn</option>
-            <option value="dreamweaver">Dreamweaver</option>
-            <option value="eclipse">Eclipse</option>
-            <option value="github">GitHub</option>
-            <option value="iplastic">IPlastic</option>
-            <option value="solarized_light">Solarized Light</option>
-            <option value="textmate">TextMate</option>
-            <option value="tomorrow">Tomorrow</option>
-            <option value="xcode">Xcode</option>
-            <option value="kuroir">Kuroir</option>
-            <option value="katzenmilch">KatzenMilch</option>
-            <option value="sqlserver">SQL Server</option>
-          </optgroup>
-        </select>
-        <label for="Language">Select A Default Language</label>
-        <select
-          name="Language"
-          id="Language"
-          v-model="user.settings.ideOptions.defaultLang"
-        >
-          <option value="python">Python</option>
-          <option value="Java">Java</option>
-        </select>
-        <label for="ConsoleForeground">Select A Console Foreground Color</label>
-        <input
-          type="color"
-          name="ConsoleForeground"
-          id="ConsoleForeground"
-          v-model="user.settings.consoleOptions.foreground"
-        />
-        <label for="ConsoleBackground">Select A Console Background Color</label>
-        <input
-          type="color"
-          name="ConsoleBackground"
-          id="ConsoleBackground"
-          v-model="user.settings.consoleOptions.background"
-        />
+        <div class="col-8">
+          <div class="profile-section">
+            <div class="editable">
+              <label for="Name">Name</label>
+              <input type="text" v-model="user.name" id="Name" />
+              <label for="ScreenName">ScreenName</label>
+              <input type="text" v-model="user.screen_name" id="ScreenName" />
+              <label for="FSCID">FSC ID</label>
+              <input type="number" v-model="user.fsc_id" id="FSCID" disabled />
+              <label for="pronouns">Preferred Pronouns</label>
+              <input type="text" name="pronouns" id="pronouns" v-model="user.pronouns" />
+            </div>
+          </div>
+          <div class="Editor-Settings">
+            <!-- theme, language, console theme -->
+            <label for="Theme">Select A Default Theme</label>
+            <select name="Theme" id="Theme" v-model="user.settings.ideOptions.theme">
+              <optgroup label="Dark">
+                <option value="ambiance">Ambiance</option>
+                <option value="chaos">Chaos</option>
+                <option value="clouds_midnight">Clouds Midnight</option>
+                <option value="dracula">Dracula</option>
+                <option value="cobalt">Cobalt</option>
+                <option value="gruvbox">Gruvbox</option>
+                <option value="gob" selected>Green on Black</option>
+                <option value="idle_fingers">idle Fingers</option>
+                <option value="kr_theme">krTheme</option>
+                <option value="merbivore">Merbivore</option>
+                <option value="merbivore_soft">Merbivore Soft</option>
+                <option value="mono_industrial">Mono Industrial</option>
+                <option value="monokai">Monokai</option>
+                <option value="nord_dark">Nord Dark</option>
+                <option value="pastel_on_dark">Pastel on dark</option>
+                <option value="solarized_dark">Solarized Dark</option>
+                <option value="terminal">Terminal</option>
+                <option value="tomorrow_night">Tomorrow Night</option>
+                <option value="tomorrow_night_blue">Tomorrow Night Blue</option>
+                <option value="tomorrow_night_bright">Tomorrow Night Bright</option>
+                <option value="tomorrow_night_eighties">Tomorrow Night 80s</option>
+                <option value="twilight">Twilight</option>
+                <option value="vibrant_ink">Vibrant Ink</option>
+              </optgroup>
+              <optgroup label="Light">
+                <option value="chrome">Chrome</option>
+                <option value="clouds">Clouds</option>
+                <option value="crimson_editor">Crimson Editor</option>
+                <option value="dawn">Dawn</option>
+                <option value="dreamweaver">Dreamweaver</option>
+                <option value="eclipse">Eclipse</option>
+                <option value="github">GitHub</option>
+                <option value="iplastic">IPlastic</option>
+                <option value="solarized_light">Solarized Light</option>
+                <option value="textmate">TextMate</option>
+                <option value="tomorrow">Tomorrow</option>
+                <option value="xcode">Xcode</option>
+                <option value="kuroir">Kuroir</option>
+                <option value="katzenmilch">KatzenMilch</option>
+                <option value="sqlserver">SQL Server</option>
+              </optgroup>
+            </select>
+            <label for="Language">Select A Default Language</label>
+            <select
+              name="Language"
+              id="Language"
+              v-model="user.settings.ideOptions.defaultLang"
+            >
+              <option value="python">Python</option>
+              <option value="Java">Java</option>
+            </select>
+            <label for="ConsoleForeground">Select A Console Foreground Color</label>
+            <input
+              type="color"
+              name="ConsoleForeground"
+              id="ConsoleForeground"
+              v-model="user.settings.consoleOptions.foreground"
+            />
+            <label for="ConsoleBackground">Select A Console Background Color</label>
+            <input
+              type="color"
+              name="ConsoleBackground"
+              id="ConsoleBackground"
+              v-model="user.settings.consoleOptions.background"
+            />
+          </div>
+        </div>
       </div>
       <button @click="saveProfile" class="btn btn-danger btn-block">Save</button>
 
