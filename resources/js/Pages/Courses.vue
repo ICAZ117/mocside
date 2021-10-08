37689fe639f62965cb762f7ba0f8205c9c4f9fc9
Vbuddies <cheesychase@myexcel.com>
2021-10-08 11:09:44 -0400

    testing calc

--- resources/js/Pages/Courses.vue
+++ resources/js/Pages/Courses.vue
@@ -269,13 +269,10 @@ export default {
         const menu = document.getElementById(this.rightClickID).childNodes[0];
         const outClick = document.getElementById("out-click");
         console.log("ClientY: " + window.event.clientY);
-        console.log("calcY" + (window.event.clientY - 236));
-        console.log("calcX" + (window.event.clientX - 277.5));
         console.log("ClientX: " + window.event.clientX);
-        menu.style.top = `${window.event.clientY}px`;
-        menu.style.left = `${window.event.clientX}px`;
+        menu.style.top = calc(`${window.event.clientY}px` - 209);
+        menu.style.left = calc(`${window.event.clientX}px` -40);
         menu.classList.add("show");
-
         outClick.style.display = "block";
       }
     },
