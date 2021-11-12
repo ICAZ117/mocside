c1b8a36226aa9c9d3a7d9d65120a54af6b8d971c
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-11-12 10:47:43 -0500

    Potential big-brain solution to repeating input?

--- resources/js/Components/Console.vue
+++ resources/js/Components/Console.vue
@@ -167,6 +167,7 @@ export default {
         console.log(e);
         if(this.enteredInput) {
           this.enteredInput = false;
+          this.oldTermContent = e.log;
         }
         else {
           this.newTermContent = e.log;
