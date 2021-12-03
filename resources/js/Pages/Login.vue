7ac002a9142d9f3ab789e1e23acbac47ebe9613c
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-12-03 11:30:36 -0500

    Debugging

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -139,10 +139,8 @@ export default {
         }
       } catch (error) {
         this.error = getError(error);
-        console.log("\n\n\nMY DEBUG:");
-        const res = console.debug({error});
-        console.log("\n\n\nRES FROM DEBUG:");
-        console.log(res);
+        console.log("\n\n\nMY ERROR STUFF:");
+        console.log(error.error);
         console.log("\n\n\nLogin Response:");
         console.log(this.error);
         console.log(this.error.error.response.data.errors.email[1]);
