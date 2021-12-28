625872acb1db789a0132d04e8335c46a7f399c72
Vbuddies <cheesychase@myexcel.com>
2021-12-28 12:27:52 -0500

    returning to old

--- resources/js/Pages/Courses.vue
+++ resources/js/Pages/Courses.vue
@@ -721,11 +721,6 @@ export default {
       }
     },
     routeToChild() {
-      console.log("route to child pathname");
-      console.log(window.location.pathname);
-      if(window.location.pathname == "/profile") {
-        console.log("should be going to profile not course page");
-      }
       var r = window.location.pathname;
       var sub = "/courses";
       var c = r.substring(sub.length);
