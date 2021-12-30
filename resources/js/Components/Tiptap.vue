2fdd49550eb215f196f7bad1a3513bca8e4b9f63
Ibraheem Cazalas <ibraheemcazalas@gmail.com>
2021-12-30 09:23:28 -0800

    Testing scrollbar on workspace description

--- resources/js/Components/Tiptap.vue
+++ resources/js/Components/Tiptap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="height: fit-content!important;">
+  <div class="dark-scroll-bar" style="height: fit-content!important; overflow-y: scroll !important;">
     <bubble-menu
       class="bubble-menu"
       :tippy-options="{ duration: 100 }"
