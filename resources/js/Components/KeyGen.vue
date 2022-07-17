8f14c50fe2b92f27aaeb41c95f3107f1b733f674
Chase Robinson <srobinson6@mocs.flsouthern.edu>
2022-07-17 15:59:23 -0400

    style adjustments to course enroll

---
+++ resources/js/Components/KeyGen.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="form-group">
+        <ul>
+        <li v-for="(k, id) in joinKeys" :key="k">
+            {{ k.join_key }}
+            <a @click="copyKey(k)">Copy Url</a>
+            <a @click="deleteKey(k, id)">Delete Key</a>
+        </li>
+        </ul>
+        <div class="key-options">
+        <label class="course-edit-label">Enroll Key</label>
+        <input
+            placeholder="Random"
+            type="text"
+            v-model="enrollKey.key"
+            class="profile-field course-edit-field"
+        />
+        <br />
+        <label class="course-edit-label">Permanent Key</label>
+        <label class="switch">
+            <input type="checkbox" v-model="enrollKey.perm" />
+            <span class="slider round"></span>
+        </label>
+        <br />
+        <label class="course-edit-label">Expire Date</label>
+        <input type="date" :disabled="enrollKey.perm" v-model="enrollKey.datetime" />
+        <br />
+        <label>Expire Time</label>
+        <input type="time" :disabled="enrollKey.perm" v-model="enrollKey.time" />
+        <br />
+        <label class="course-edit-label">Max Uses</label>
+        <input
+            placeholder="0 for unlimited use"
+            type="text"
+            v-model="enrollKey.uses"
+            class="profile-field course-edit-field"
+        />
+        </div>
+        <button type="button" @click="generateKey" class="btn btn-danger btn-block">
+        Generate Course Enroll Key
+        </button>
+    </div>
+</template>
+
+<script>
+import * as API from "../../services/API";
+export default {
+    props: ["courseID"],
+    data() {
+        return {
+            enrollKey: {
+                key: "",
+                perm: true,
+                datetime: "",
+                time: "",
+                uses: "",
+            },
+            joinKeys: [],
+            keyURL: "",
+        }
+    },
+    methods: {
+        fetchKeys() {
+            const res = await API.apiClient.get(`/invite/course/${this.courseID}`);
+            var myArr = res.data.data;
+            for (let i = 0; i < myArr.length; i++) {
+                this.joinKeys.push(myArr[i]);
+            }
+        },
+        copyKey(key) {
+            this.keyURL = "http://mocside.com:8000/" + key.join_key + "/enroll";
+            //copy to clipboard
+            var copyText = this.keyURL;
+            const textarea = document.createElement("textarea");
+            textarea.value = copyText;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand("copy");
+            document.body.removeChild(textarea);
+        },
+        async deleteKey(key, id) {
+            //call delete api method
+            const res = await API.apiClient.delete(`/invite/${key.id}`);
+            //filter from front end
+            this.joinKeys = this.joinKeys.filter((k, i) => i != id);
+        },
+        async generateKey() {
+            var payload = {};
+            if (this.enrollKey.key == "") {
+                this.enrollKey.key = "random";
+            }
+            payload["join_key"] = this.enrollKey.key;
+            payload["course_id"] = this.courseID;
+            const res = await API.apiClient.post(`/invite`, payload);
+            var keyCode = res.data.data.id;
+            payload["join_key"] = res.data.data.join_key;
+            if (this.enrollKey.perm) {
+                //get end time of course
+                payload["expire_date"] = this.courseForm.dateEnd;
+            } else {
+                //expire date is set to datetime
+                payload["expire_date"] = this.enrollKey.datetime + " " + this.enrollKey.time;
+            }
+            if (this.enrollKey.uses == "") {
+                this.enrollKey.uses = 0;
+            }
+            payload["max_uses"] = this.enrollKey.uses;
+            const res2 = await API.apiClient.put(`/invite/${keyCode}`, payload);
+            this.joinKeys.push(res2.data.data);
+        },
+    },
+    mounted() {
+        await this.fetchKeys();
+    }
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/Pages/Courses/EditCourse.vue
+++ resources/js/Pages/Courses/EditCourse.vue
@@ -118,47 +118,7 @@
 
     <div class="bottom-row">
       <div class="bottom-left key-gen">
-        <div class="form-group">
-          <ul>
-            <li v-for="(k, id) in joinKeys" :key="k">
-              {{ k.join_key }}
-              <a @click="copyKey(k)">Copy Url</a>
-              <a @click="deleteKey(k, id)">Delete Key</a>
-            </li>
-          </ul>
-          <div class="key-options">
-            <label class="course-edit-label">Enroll Key</label>
-            <input
-              placeholder="Random"
-              type="text"
-              v-model="enrollKey.key"
-              class="profile-field course-edit-field"
-            />
-            <br />
-            <label class="course-edit-label">Permanent Key</label>
-            <label class="switch">
-              <input type="checkbox" v-model="enrollKey.perm" />
-              <span class="slider round"></span>
-            </label>
-            <br />
-            <label class="course-edit-label">Expire Date</label>
-            <input type="date" :disabled="enrollKey.perm" v-model="enrollKey.datetime" />
-            <br />
-            <label>Expire Time</label>
-            <input type="time" :disabled="enrollKey.perm" v-model="enrollKey.time" />
-            <br />
-            <label class="course-edit-label">Max Uses</label>
-            <input
-              placeholder="0 for unlimited use"
-              type="text"
-              v-model="enrollKey.uses"
-              class="profile-field course-edit-field"
-            />
-          </div>
-          <button type="button" @click="generateKey" class="btn btn-danger btn-block">
-            Generate Course Enroll Key
-          </button>
-        </div>
+        <KeyGen :courseID="courseID"></KeyGen>
       </div>
       <div class="bottom-right labs">
         <div
@@ -224,6 +184,7 @@ import { getError } from "../../utils/helpers";
 import FileService from "../../services/FileService";
 import FlashMessage from "../../Components/FlashMessage";
 import FileUpload from "../../Components/FileUpload";
+import KeyGen from "../../Components/KeyGen.vue";
 import store from "../../Store/index";
 export default {
   props: ["courseID"],
@@ -231,6 +192,7 @@ export default {
   components: {
     FlashMessage,
     FileUpload,
+    KeyGen,
   },
   data() {
     return {
@@ -270,54 +232,6 @@ export default {
     };
   },
   methods: {
-    async initKeys() {
-      const res = await API.apiClient.get(`/invite/course/${this.courseID}`);
-      var myArr = res.data.data;
-      for (let i = 0; i < myArr.length; i++) {
-        this.joinKeys.push(myArr[i]);
-      }
-    },
-    async generateKey() {
-      var payload = {};
-      if (this.enrollKey.key == "") {
-        this.enrollKey.key = "random";
-      }
-      payload["join_key"] = this.enrollKey.key;
-      payload["course_id"] = this.courseID;
-      const res = await API.apiClient.post(`/invite`, payload);
-      var keyCode = res.data.data.id;
-      payload["join_key"] = res.data.data.join_key;
-      if (this.enrollKey.perm) {
-        //get end time of course
-        payload["expire_date"] = this.courseForm.dateEnd;
-      } else {
-        //expire date is set to datetime
-        payload["expire_date"] = this.enrollKey.datetime + " " + this.enrollKey.time;
-      }
-      if (this.enrollKey.uses == "") {
-        this.enrollKey.uses = 0;
-      }
-      payload["max_uses"] = this.enrollKey.uses;
-      const res2 = await API.apiClient.put(`/invite/${keyCode}`, payload);
-      this.joinKeys.push(res2.data.data);
-    },
-    copyKey(key) {
-      this.keyURL = "http://mocside.com:8000/" + key.join_key + "/enroll";
-      //copy to clipboard
-      var copyText = this.keyURL;
-      const textarea = document.createElement("textarea");
-      textarea.value = copyText;
-      document.body.appendChild(textarea);
-      textarea.select();
-      document.execCommand("copy");
-      document.body.removeChild(textarea);
-    },
-    async deleteKey(key, id) {
-      //call delete api method
-      const res = await API.apiClient.delete(`/invite/${key.id}`);
-      //filter from front end
-      this.joinKeys = this.joinKeys.filter((k, i) => i != id);
-    },
     async handleSubmit() {
       this.isSubmitted = true;
       await this.uploadImage();
@@ -515,7 +429,6 @@ export default {
     this.courseForm.roster = JSON.parse(this.course.data.data.roster).roster;
     this.course = this.course.data.data;
     this.getStudents();
-    this.initKeys();
     await this.getLabs();
   },
   beforeUnmount() {
