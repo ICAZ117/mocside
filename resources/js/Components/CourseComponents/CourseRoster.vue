3beb1cce4f313bab07510ff81a3a063ccfd29195
Chase Robinson <srobinson6@mocs.flsouthern.edu>
2022-07-20 22:54:44 -0400

    adding commented button

--- resources/js/Components/CourseComponents/CourseRoster.vue
+++ resources/js/Components/CourseComponents/CourseRoster.vue
@@ -30,6 +30,9 @@
             </table>
         </div>
         <div class="student-view-btn flex-row" style="display: flex; justify-content: end;">
+            <!-- <button type="button" @click="addStudent()" class="btn btn-danger btn-block add-student-btn">
+                Add Student
+            </button> -->
             <button type="button" @click="studentView()" class="btn btn-danger btn-block student-view-btn">
                 Student View
             </button>
