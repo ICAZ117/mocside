529eada9a1c56acd9f3393ddc11f81ecfa35e100
Stephen Chase Robinson <cheesychase@myexcel.com>
2022-07-28 22:54:26 -0400

    pls work

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -167,15 +167,15 @@ export default {
     $route(to) {
 		console.log("watch")
 		console.log(to)
-      if (to.fullPath != "/") {
-        this.isDark = true;
-      } else {
+      if (to.fullPath == "/") {
         this.isDark = false;
+      } else {
+        this.isDark = true;
       }
       this.updatePFP();
-      this.$emit("forceReload");
-      if(to.fullPath == "/profile") {
-      }
+    //   this.$emit("forceReload");
+    //   if(to.fullPath == "/profile") {
+    //   }
     },
   },
 };
