6e9ba8abbf5cd9bd7adebf76f0a2f165faee2c2e
Chase Robinson <srobinson6@mocs.flsouthern.edu>
2022-07-31 14:51:09 -0400

    overhaul workspace

--- resources/js/Pages/WorkSpace/Workspace.vue
+++ resources/js/Pages/WorkSpace/Workspace.vue
@@ -1,17 +1,14 @@
 <template>
-  <div v-if="childIsOpen" class="workspace-page">
-    <!-- WorkSpace Page-->
-    <!-- <button v-if="childIsOpen" @click="unmountingWork()" class="btn btn-danger btn-block">
-      Return to Problems
-    </button> -->
-    <div v-if="childIsOpen">
+  <div class="workspace-page">
+    <!-- Workspace page -->
+    <div>
       <Vue3DraggableResizable
         :initW="instructionsWidth"
         :initH="instructionsHeight"
-        v-model:x="x1"
-        v-model:y="y1"
-        v-model:w="w1"
-        v-model:h="h1"
+        v-model:x="instructx"
+        v-model:y="instructy"
+        v-model:w="instructw"
+        v-model:h="instructh"
         v-model:active="active"
         :draggable="false"
         :resizable="true"
@@ -45,13 +42,14 @@
           v-if="childIsOpen"
         />
       </Vue3DraggableResizable>
+
       <Vue3DraggableResizable
         :initW="IDEWidth"
         :initH="IDEHeight"
-        v-model:x="x2"
-        v-model:y="y2"
-        v-model:w="w2"
-        v-model:h="h2"
+        v-model:x="IDEx"
+        v-model:y="IDEy"
+        v-model:w="IDEw"
+        v-model:h="IDEh"
         v-model:active="active"
         :draggable="false"
         :resizable="true"
@@ -61,7 +59,7 @@
         :key="reloadSliders"
         id="resizeableIDE"
       >
-        <IDE
+        <!-- <IDE
           :offsetTop="0"
           :lang="lang"
           :showSubmit="true"
@@ -74,7 +72,7 @@
           :key="forceReload"
           v-if="childIsOpen"
           :width="debouncedWidth"
-        />
+        /> -->
       </Vue3DraggableResizable>
     </div>
   </div>
@@ -82,126 +80,62 @@
 
 <script>
 import * as API from "../../services/API";
-import { defineComponent } from "vue";
 import Vue3DraggableResizable from "vue3-draggable-resizable";
-//default styles
 import "vue3-draggable-resizable/dist/Vue3DraggableResizable.css";
-
-export default defineComponent({
+export default {
   props: ["problemID", "lang", "labID"],
-  emits: ["problemEdited", "deleteMe"],
   components: { Vue3DraggableResizable },
-
-  data() {
+  data () {
     return {
-      instructionsHeight: window.innerHeight - 60,
-      instructionsWidth: window.innerWidth * 0.33,
-      IDEHeight: window.innerHeight - 60,
-      IDEWidth: window.innerWidth * 0.67,
-      assignment: {},
-      title: "",
+      assignment: null,
+      title: null,
       description: null,
-      code_j: "",
-      code_p: "",
-      theme: "",
-      input: "",
-      jID: "",
-      pID: "",
       codeID: "",
+      code_p: null,
+      code_j: null,
       forceReload: 0,
       reloadSliders: 0,
-      childIsOpen: false,
-      saveStatus: "",
-      test: {},
-      x1: 0,
-      y1: 60,
-      h1: 0,
-      w1: 0,
-      x2: window.innerWidth * 0.33,
-      y2: 60,
-      h2: 0,
-      w2: window.innerWidth * 0.67,
+
+      // vue resizeable
+      instructionsHeight: window.innerHeight - 60,
+      instrcutionsWidth: window.innerWidth * 0.33,
+      IDEHeight: window.innerHeight - 60,
+      IDEWidth: window.innerWidth * 0.67,
+      instrcutx: 0,
+      instructy: 60,
+      instructw: 0,
+      instructh: 0,
+      IDEx: window.innerWidth * 0.33,
+      IDEy: 60,
+      IDEw: window.innerWidth * 0.67,
+      IDEh: 0,
       active: true,
       debouncedWidth: window.innerWidth * 0.67,
-      savedText: null,
-    };
-  },
-  watch: {
-    w2: function () {
-      this.w1 = window.innerWidth - this.w2;
-    },
-    active: function () {
-      this.active = true;
-    },
+    }
   },
   methods: {
-    resizeEnd() {
-      this.debouncedWidth = this.w2;
-    },
-    async unmountingWork() {
-      //go back in router by 1
-      this.$router.go(-1);
-    },
-	returnToProblems() {
-		this.$router.go(-1);
-	},
-    async getAssignment() {
-      //this route needs to be worked on and adjusted
-      const rawAssignment = await API.apiClient.get(`/problems/full/${this.problemID}`);
-      this.assignment = rawAssignment.data.data;
+    //fetching problem and work
+    async fetchAssignment() {
+      const res = await API.apiClient.get(`/problems/full/${this.problemID}`);
+      this.assignment = res.data.data;
       this.title = this.assignment.name;
       this.description = await JSON.parse(this.assignment.description);
-      //check if empty....post if empty
-      var payload = {
-        lang: this.lang,
-      };
-      const empty = await API.apiClient.post(`/code/check/${this.problemID}`, payload);
-      this.test = empty;
-      if (
-        this.assignment.python_starter == "" ||
-        this.assignment.python_starter == null
-      ) {
-        this.assignment.python_starter = "def main():\n\nmain()";
-      }
-      if (this.assignment.java_starter == "" || this.assignment.java_starter == null) {
-        this.assignment.java_starter =
-          "public class Main {\n\tpublic static void main(String[] args) {\n\t\t}}";
-      }
-      if (empty.data.message == "No progress. Please create.") {
-        //create progress
-        if (this.lang == "Java") {
-          payload = {
-            lang: this.lang.toLowerCase(0),
-            problem_id: this.problemID,
-            code: this.assignment.java_starter,
-          };
-        } else {
-          payload = {
-            lang: this.lang.toLowerCase(0),
-            problem_id: this.problemID,
-            code: this.assignment.python_starter,
-          };
-        }
-        const initial = await API.apiClient.post(`/code`, payload);
-        if (this.lang == "Java") {
-          this.code_j = this.assignment.java_starter;
-          this.codeID = initial.data.id;
-        } else {
-          this.code_p = this.assignment.python_starter;
-          this.codeID = initial.data.id;
-        }
-      } else {
-        //otherwise not empty
-        if (this.lang == "Java") {
-          this.code_j = empty.data.dump[0].code;
-          this.codeID = empty.data.dump[0].id;
-        } else {
-          this.code_p = empty.data.dump[0].code;
-          this.codeID = empty.data.dump[0].id;
-        }
-      }
-      this.forceReload = 1;
 
+      //check if no code from student or template
+      const codeCheck = await this.checkForCode();
+      if(codeCheck.data.message == "No progress. Please create.") {
+        //need to create the code
+        await this.initEmptyCode();
+      } else {
+        if (this.lang == "Java") {
+          this.code_j = codeCheck.data.dump[0].code;
+          this.codeID = codeCheck.data.dump[0].id;
+        } else {
+          this.code_p = codeCheck.data.dump[0].code;
+          this.codeID = codeCheck.data.dump[0].id;
+        }
+      }
+      this.forceReload += 1;
       // const res = await API.apiClient.get(`/code/search/${this.problemID}`);
       // var progress = res.data.data;
       // this.code_j = await this.getJava(progress);
@@ -214,91 +148,80 @@ export default defineComponent({
       // }
       // this.forceReload = 1;
     },
-    async getJava(progress) {
-      //if first time opening grab template, else grab student code
+    async checkForCode() {
       var payload = {
-        lang: "java",
-        problem_id: this.problemID,
-        code: this.assignment.java_starter,
-      };
-      if (progress.length == 0) {
-        const res = await API.apiClient.post(`/code`, payload);
-        this.jID = res.data.id;
-        return this.assignment.java_starter;
-      } else {
-        for (let i = 0; i < progress.length; i++) {
-          if (progress[i].lang == "java") {
-            this.jID = progress[i].id;
-            return progress[i].code;
-          }
-        }
-        const res = await API.apiClient.post(`/code`, payload);
-        this.jID = res.data.id;
-        return this.assignment.java_starter;
+        lang: this.lang,
       }
+      return await API.apiClient.post(`/code/check/${this.problemID}`, payload);
     },
-    async getPython(progress) {
-      //if first time opening grab template, else grab student code
-      var payload = {
-        lang: "python",
-        problem_id: this.problemID,
-        code: this.assignment.python_starter,
-      };
-      if (progress.length == 0) {
-        const res = await API.apiClient.post(`/code`, payload);
-        this.pID = res.data.id;
-        return this.assignment.python_starter;
-      } else {
-        for (let i = 0; i < progress.length; i++) {
-          if (progress[i].lang == "python") {
-            this.pID = progress[i].id;
-            return progress[i].code;
-          }
-        }
-        const res = await API.apiClient.post(`/code`, payload);
-        this.pID = res.data.id;
-        return this.assignment.python_starter;
+    async initEmptyCode() {
+      if(this.assignment.python_starter == "" || this.assignment.python_starter == null) {
+        this.assignment.python_starter = "def main():\n\nmain()";
       }
+      if(this.assignment.java_starter == "" || this.assignment.java_starter == null) {
+        this.assignment.java_starter = "public class Main {\n\tpublic static void main(String[] args) {\n\t\t}}";
+      }
+
+      payload = {
+        lang: this.lang.toLowerCase(0),
+        problem_id: this.problemID, 
+        code: this.lang == "Java" ? this.assignment.java_starter : this.assignment.python_starter,
+      };
+      const res = await API.apiClient.post(`/code`, payload);
+
+      this.codeID = res.data.id; 
+      this.code_j = this.assignment.java_starter;
+      this.code_p = this.assignment.python_starter;
     },
+
+    //updating content
     updateContent(e) {
-      this.saveStatus = "Saving...";
-      if (this.lang == "Java") {
+      if(this.lang == "Java")
         this.code_j = e.code;
-      } else {
+      else 
         this.code_p = e.code;
-      }
+
       this.timeout();
     },
-    timeout: _.debounce(async function () {
-      var payload = {};
-      if (this.lang == "Java") {
-        payload = {
-          code: this.code_j,
-        };
-        const res = await API.apiClient.put(`/code/${this.codeID}`, payload);
-      } else {
-        payload = {
-          code: this.code_p,
-        };
-        const res = await API.apiClient.put(`/code/${this.codeID}`, payload);
-        this.saveStatus = "All changes have been saved";
+    timeout: _.debounce(async function  () {
+      var payload = {
+        code: this.lang == "Java" ? this.code_j : this.code_p,
       }
+      const res = await API.apiClient.put(`/code/${this.codeID}`, payload);
     }, 1000),
+
+    //routing functions
+    returnToProblems() {
+      this.$router.go(-1);
+    },
+
+    //resizeable stuff
+    resizeEnd() {
+      this.debouncedWidth = this.IDEw;
+    },
   },
-  beforeUnmount() {
-    // workspace
-    this.childIsOpen = false;
-    document.getElementById("thisIsTheDumbestThingIHaveEverHadToDo").style = "";
+  watch: {
+    IDEw: function () {
+      this.instructw = window.innerWidth - this.IDEw;
+    },
+    active: function () {
+      this.active = true;
+    }
   },
-  async created() {
-    this.childIsOpen = true;
-    await this.getAssignment();
-  },
-  mounted() {
-    this.reloadSliders++;
-    document.getElementById("resizeableInstructions").remove();
-    document.getElementById("thisIsTheDumbestThingIHaveEverHadToDo").style =
-      "overflow: hidden!important";
-  },
-});
+  // beforeUnmount() {
+  //   // workspace
+  //   document.getElementById("thisIsTheDumbestThingIHaveEverHadToDo").style = "";
+  // },
+  async mounted() {
+    await this.fetchAssignment();
+    // this.reloadSliders++;
+    // document.getElementById("resizeableInstructions").remove();
+    // document.getElementById("thisIsTheDumbestThingIHaveEverHadToDo").style =
+    //   "overflow: hidden!important";
+  }
+}
 </script>
+
+<style>
+
+</style>
